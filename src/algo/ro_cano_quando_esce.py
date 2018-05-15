047c1f127607bba97a306782c084fa2ac34e2f89
renasboy <renasboy@gmail.com>
2018-05-15 21:09:38 +0200

    FIX new algo for ro_cano_quand_esce

--- src/algo/ro_cano_quando_esce.py
+++ src/algo/ro_cano_quando_esce.py
@@ -5,6 +5,7 @@ class ro_cano_quando_esce(object):
     def __init__(self, helper):
         self.algo_helper = helper
         self.session = 0
+        self.open = False
 
     @property
     def action(self):
@@ -14,6 +15,7 @@ class ro_cano_quando_esce(object):
         ma2_last, ma2_prev = self.algo_helper.ma_last_prev(2)
         ma3_last, ma3_prev = self.algo_helper.ma_last_prev(3)
         ma5_last, ma5_prev = self.algo_helper.ma_last_prev(5)
+        ma6_last, ma6_prev = self.algo_helper.ma_last_prev(6)
         ma8_last, ma8_prev = self.algo_helper.ma_last_prev(8)
         ma9_last, ma9_prev = self.algo_helper.ma_last_prev(9)
         ma10_last, ma10_prev = self.algo_helper.ma_last_prev(10)
@@ -32,18 +34,18 @@ class ro_cano_quando_esce(object):
 
         # apre la gabbia, se subito dopo l'incrocio della ma8 X ma31 la ma8 >= ma31
         if ma31_prev and ma31_last and ma8_prev < ma31_prev and ma8_last >= ma31_last:
-            self.session = 1
+            self.open = True
+            if not self.session:
+                self.session = 1
             self.algo_helper.log('session {}: open segment'.format(self.session))
+        # se chiude la gabbia, vende subito
+        # subito dopo l'incrocio della ma8 X ma31 la m8 < ma31
+        elif ma31_prev and ma31_last and ma8_prev >= ma31_prev and ma8_last < ma31_last:
+            self.open = False
+            self.algo_helper.log('session {}: closed segment'.format(self.session))
 
         # compra o vende solo se ma8 >= ma31
-        if self.session and ma8_last >= ma31_last:
-
-            # salvagente, vende subito se il prezzo < last_trade_price - 0.35%
-            if last_trade_action == 'buy' and price < last_trade_price - last_trade_price * 0.0035:
-                action = 'sell'
-
-            # compra
-            elif last_trade_action != 'buy':
+        if self.open and self.session and last_trade_action != 'buy' and ma8_last >= ma31_last:
 
                 # compra sessione UNO solo se
                 # subito
@@ -51,8 +53,8 @@ class ro_cano_quando_esce(object):
                     action = 'buy'
 
                 # compra sessione DUE solo se
-                # subito dopo l'incrocio del ma2 X ma9 il ma2 > ma9
-                elif self.session == 2 and ma2_prev < ma9_prev and ma2_last > ma9_last:
+                # subito dopo l'incrocio del ma3 X ma10 il ma3 > ma10
+                elif self.session == 2 and ma3_prev < ma10_prev and ma3_last > ma10_last:
                     action = 'buy'
                 
                 # compra sessione X solo se
@@ -60,45 +62,43 @@ class ro_cano_quando_esce(object):
                 elif self.session > 2 and ma3_prev < ma11_prev and ma3_last > ma11_last:
                     action = 'buy'
 
-            # vende
-            elif last_trade_action == 'buy':
+        # vende
+        elif last_trade_action == 'buy':
 
-                # vende sessione UNO solo se
-                # subito dopo l'incrocio della ma1 X ma10 la ma1 < ma10
-                if self.session == 1 and ma1_prev > ma10_prev and ma1_last < ma10_last and (datetime.now() - datetime.strptime(last_trade_time[:-11], '%Y-%m-%dT%H:%M:%S')).seconds > 60:
-                    action = 'sell'
+            # salvagente, vende subito se il prezzo < last_trade_price - 0.35%
+            if last_trade_action == 'buy' and price < last_trade_price - last_trade_price * 0.0035:
+                action = 'sell'
 
-                # vende sessione DUE solo se
-                # subito dopo l'incrocio prezzo X ma11 il prezzo < ma11
-                elif self.session == 2 and ma1_prev > ma11_prev and ma1_last < ma11_last and (datetime.now() - datetime.strptime(last_trade_time[:-11], '%Y-%m-%dT%H:%M:%S')).seconds > 60:
-                    action = 'sell'
+            # vende sessione UNO solo se
+            # subito dopo l'incrocio della ma1 X ma6 la ma1 < ma6
+            if self.session == 1 and ma1_prev > ma6_prev and ma1_last < ma6_last and (datetime.now() - datetime.strptime(last_trade_time[:-11], '%Y-%m-%dT%H:%M:%S')).seconds > 60:
+                action = 'sell'
 
-                # vende session X solo se
-                # subito dopo l'incrocio prezzo X ma11 il prezzo < ma11
-                elif self.session > 2 and ma1_prev > ma11_prev and ma1_last < ma11_last:
-                    action = 'sell'
+            # vende sessione DUE solo se
+            # subito dopo l'incrocio prezzo X ma11 il prezzo < ma11
+            elif self.session == 2 and ma1_prev > ma11_prev and ma1_last < ma11_last and (datetime.now() - datetime.strptime(last_trade_time[:-11], '%Y-%m-%dT%H:%M:%S')).seconds > 60:
+                action = 'sell'
 
-                # fascia di non vendita
-                if action == 'sell':
-                    # ma anche solo se guadagno > 0.09%
-                    if price - last_trade_price >= 0 and price - last_trade_price < last_trade_price * 0.0009:
-                        action = None
-                    # perdita < -0.33%
-                    elif price - last_trade_price <= 0 and last_trade_price - price < last_trade_price * 0.0033:
-                        action = None
+            # vende session X solo se
+            # subito dopo l'incrocio prezzo X ma11 il prezzo < ma11
+            elif self.session > 2 and ma1_prev > ma11_prev and ma1_last < ma11_last:
+                action = 'sell'
 
-            self.algo_helper.log('session {}: action {}'.format(self.session, action))
-
+            # fascia di non vendita
             if action == 'sell':
-                self.algo_helper.log('session {}: closed session'.format(self.session))
-                self.session = self.session + 1
+                # ma anche solo se guadagno > 0.09%
+                if price - last_trade_price >= 0 and price - last_trade_price < last_trade_price * 0.0009:
+                    action = None
+                # perdita < -0.33%
+                elif price - last_trade_price <= 0 and last_trade_price - price < last_trade_price * 0.0033:
+                    action = None
 
-        # se chiude la gabbia, vende subito
-        # subito dopo l'incrocio della ma8 X ma31 la m8 < ma31
-        elif self.session and ma8_prev >= ma31_prev and ma8_last < ma31_last:
-            self.algo_helper.log('session {}: closed segment'.format(self.session))
-            self.session = 0
-            #if last_trade_action == 'buy':
-            #    action = 'sell'
+        self.algo_helper.log('session {}: action {}'.format(self.session, action))
+
+        if action == 'sell':
+            self.algo_helper.log('session {}: closed session'.format(self.session))
+            self.session = self.session + 1
+            if not self.open:
+                self.session = 0
 
         return action
