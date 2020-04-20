9d5776a23d9a630dde94f57a5e46a15168442664
DNABM <36816435+DNABM@users.noreply.github.com>
2020-04-20 12:31:06 +0200

    riga 61 - modificata compra 1 + condizione speciale + aggiunta price>ma43
    COMPRA 1 ma8>ma34 con MACD < 1,2
    CONDIZIONE SPECIALE (ma7/ma34) -1 x 100 >0,26 con MACD >1
    AGGIUNTA ALTRA CONDIZIONE price>ma43

--- src/algo/ro_cano_quando_esce.py
+++ src/algo/ro_cano_quando_esce.py
@@ -57,8 +57,8 @@ class ro_cano_quando_esce(object):
             self.open = False
             self.algo_helper.log('session {}: closed segment'.format(self.session))
 
-        # compra o vende solo se ma8 >= ma33 ed anche (macd proper > 3.4 oppure se ((ma7 / ma33 - 1) * 100 > 0.26) and macd > 1)
-        if self.open and self.session and last_trade_action != 'buy' and ma8_last >= ma33_last and (macd > 3.4 or ((ma7_last / ma33_last - 1) * 100 > 0.26) and macd > 1):
+        # compra o vende solo se ma8 >= ma34 ed anche (macd proper < 1.2 oppure se ((ma7 / ma34 - 1) * 100 > 0.26) and macd > 1)
+        if self.open and self.session and last_trade_action != 'buy' and ma8_last >= ma34_last and (macd < 1.2 or ((ma7_last / ma34_last - 1) * 100 > 0.26) and macd > 1) and ma1_last > ma34_last:
 
                 # compra sessione UNO solo se
                 # subito
