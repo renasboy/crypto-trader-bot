d50342c621803a13c913563ac38f4c3d2358f770
DNABM <36816435+DNABM@users.noreply.github.com>
2020-05-03 20:01:15 +0200

    riga 119 - piccola correzione

--- src/algo/ro_cano_quando_esce.py
+++ src/algo/ro_cano_quando_esce.py
@@ -116,7 +116,7 @@ class ro_cano_quando_esce(object):
 
             # vende subito se la gabbia e' chiusa
             # "condizione corona" se MACD >25 vendi subito
-            # vende subito anche se dopo 50 minuti ma8<ma34 e ma34>ma43
+            # vende subito anche se dopo 50 minuti ma8<ma34 e ma34<ma43
             
             if not self.open or macd > 25:
                 action = 'sell'
