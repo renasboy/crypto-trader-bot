3de8ddb500198adf9d60fab81cb83b8678fa28a8
DNABM <36816435+DNABM@users.noreply.github.com>
2020-05-17 14:18:51 +0200

    MACD compra 1 portato a <3

--- src/algo/ro_cano_quando_esce.py
+++ src/algo/ro_cano_quando_esce.py
@@ -76,10 +76,10 @@ class ro_cano_quando_esce(object):
             self.open = False
             self.algo_helper.log('session {}: closed segment'.format(self.session))
 
-        # compra o vende solo se ma8 >= ma34 ed anche (macd proper <= -1.0 oppure se ((ma8 / ma34 - 1) * 100 > 0.37) and macd < -2.0)
+        # compra o vende solo se ma8 >= ma34 ed anche (macd proper <= 3.0 oppure se ((ma8 / ma34 - 1) * 100 > 0.37) and macd < -2.0)
         # speciale: macd > macd_4_min_ago e ma8_last > ma8_10_min_ago
-        # macd <= -1.0 a macd < -2.0
-        # and (macd < -1.0 or ((ma7_last / ma34_last - 1) * 100 > 0.37) and macd < -30)
+        # macd <= 3.0 a macd < -2.0
+        # and (macd < 3.0 or ((ma7_last / ma34_last - 1) * 100 > 0.37) and macd < -30)
         if (self.open and self.session and last_trade_action != 'buy'
             and macd >= macd_4_min_ago
             and macd > macd_3_min_ago
@@ -92,7 +92,7 @@ class ro_cano_quando_esce(object):
             and ma5_last >= ma5_3_min_ago
             and ma8_last >= ma34_last
             and ma1_last > ma34_last
-            and (macd <= -1.0 or ((ma7_last / ma34_last - 1) * 100 > 0.37) and macd < -30.0)
+            and (macd <= 3.0 or ((ma7_last / ma34_last - 1) * 100 > 0.37) and macd < -30.0)
             and ma1_last > ma34_last):
 
                 # compra sessione UNO solo se
