7f8617c97de6c2d9e8d3da486922dde511601549
DNABM <36816435+DNABM@users.noreply.github.com>
2020-05-23 16:12:01 +0200

    compra 1 - aggiunto "and ma34_last > ma34_2_min_ago"

--- src/algo/ro_cano_quando_esce.py
+++ src/algo/ro_cano_quando_esce.py
@@ -43,6 +43,7 @@ class ro_cano_quando_esce(object):
         ma2_1_min_ago = self.algo_helper.ma_minutes_ago(2, 1)
         ma100_2_min_ago = self.algo_helper.ma_minutes_ago(100, 2)
         ma100_3_min_ago = self.algo_helper.ma_minutes_ago(100, 3)
+        ma34_2_min_ago = self.algo_helper.ma_minutes_ago(34, 2)
         
         # LAST TRADE
         last_trade_action = self.algo_helper.last_trade_action
@@ -91,6 +92,7 @@ class ro_cano_quando_esce(object):
             and ma8_last > ma8_2_min_ago
             and ma8_last > ma8_3_min_ago
             and ma8_last > ma8_5_min_ago
+            and ma34_last > ma34_2_min_ago
             and ma43_last > ma43_2_min_ago
             and ma43_last >= ma43_5_min_ago
             and ma100_last >= ma100_2_min_ago
