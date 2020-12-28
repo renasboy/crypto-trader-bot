1f7b07e7137d55c7c28ef57dd5142746da82b2e0
DNABM <36816435+DNABM@users.noreply.github.com>
2020-12-29 00:22:38 +0100

    Problemi di rispetto alle condizioni sell
    Cancellata
    macd4minago<macd5minago

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -173,20 +173,20 @@ class ro_cano_che_ritorna(object):
             if self.session == 1:
                 if (macd < macd_1_min_ago
                     and macd < macd_3_min_ago
-                    and macd_1_min_ago < macd_2_min_ago
-                    and macd_4_min_ago < macd_5_min_ago):
+                    and macd_1_min_ago < macd_2_min_ago):
+                    
                     action = 'sell'
             elif self.session == 2:
                 if (macd < macd_1_min_ago
                     and macd < macd_3_min_ago
-                    and macd_1_min_ago < macd_2_min_ago
-                    and macd_4_min_ago < macd_5_min_ago):
+                    and macd_1_min_ago < macd_2_min_ago):
+                    
                     action = 'sell' 
             else:
                 if (macd < macd_1_min_ago
                     and macd < macd_3_min_ago
-                    and macd_1_min_ago < macd_2_min_ago
-                    and macd_4_min_ago < macd_5_min_ago):
+                    and macd_1_min_ago < macd_2_min_ago):
+           
                     action = 'sell' 
             
             
