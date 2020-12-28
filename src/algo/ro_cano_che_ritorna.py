1351bcb920f5f1e5b3b8192972d20f2a799ac0ac
DNABM <36816435+DNABM@users.noreply.github.com>
2020-12-28 11:57:37 +0100

    Update ro_cano_che_ritorna.py

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -84,7 +84,7 @@ class ro_cano_che_ritorna(object):
    
         
         if macd_2_min_ago and macd > macd_2_min_ago
-           and ma8_last > ma8_3_min_ago:
+            and ma8_last > ma8_3_min_ago:
             #"and if" ma20_last > ma20_3_min_ago
             #"and if" ma100_last > ma100_5_min_ago
             #"and if" ma200_last > ma200_3_min_ago
