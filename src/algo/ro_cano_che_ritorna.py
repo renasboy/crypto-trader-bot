8c2d09ed827cb7c42477fedd3fa1b64bbe74f77f
DNABM <36816435+DNABM@users.noreply.github.com>
2021-01-04 22:29:53 +0100

    deviation da < -0.60: a < -0.80:

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -209,7 +209,7 @@ class ro_cano_che_ritorna(object):
             #self.algo_helper.log('MACD: {}'.format(macd)) questa riga fa comparire la variabile sul log
             if self.session == 1:
                 if ma2_prev > ma4_prev and ma2_last < ma4_last:
-                    if deviation > 0.20 or deviation < -0.60: 
+                    if deviation > 0.20 or deviation < -0.80: 
                         
                         action = 'sell'
                     
@@ -246,7 +246,7 @@ class ro_cano_che_ritorna(object):
                         
             elif self.session == 2:   
                 if ma2_prev > ma4_prev and ma2_last < ma4_last:
-                    if deviation > 0.20 or deviation < -0.60: 
+                    if deviation > 0.20 or deviation < -0.80: 
                         action = 'sell'     
                     
                     
@@ -261,7 +261,7 @@ class ro_cano_che_ritorna(object):
                         
             else:
                 if (ma2_prev > ma4_prev and ma2_last < ma4_last):
-                    if deviation > 0.20 or deviation < -0.60: 
+                    if deviation > 0.20 or deviation < -0.80: 
                         action = 'sell'
                     
                     #ma2_prev > ma5_prev and ma2_last < ma5_last):
