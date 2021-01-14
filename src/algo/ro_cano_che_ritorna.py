7f1eaef97768aedf57af6a314c9afbc9dde96bcc
DNABM <36816435+DNABM@users.noreply.github.com>
2021-01-14 12:42:40 +0100

    riga ( 132-133-99-24 ) aiutami tu compa

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -21,7 +21,7 @@ class ro_cano_che_ritorna(object):
         # ma anche per comprare UN PO' PIU' SOPRA DELL' ULTIMO BUY
         
         min_buy_delay_in_seconds = 540
-        
+        # forse bisogna mettere un contatore per il prev
         
         # MACD di 1-2-3-4 minuti prima
         macd = self.algo_helper.macd
@@ -96,7 +96,7 @@ class ro_cano_che_ritorna(object):
         
         
         
-        # deviation prev_trade (qualche volta il BUY)
+        # deviation prev_trade (qualche volta il BUY - CASO ECCEZIONALE ) vedi riga 131
         # deviation = (price / prev_trade_price - 1) * 100 if prev_trade_price else 0
         
         
@@ -128,9 +128,9 @@ class ro_cano_che_ritorna(object):
             if ((seconds_since_last_trade > 0 and seconds_since_last_trade <= min_buy_delay_in_seconds and deviation > 0.4)
                 or (seconds_since_last_trade == 0 or seconds_since_last_trade > min_buy_delay_in_seconds)):
                 
-            # riga 131 - riga 65 -COMPRA UN PO' PIU' SOPRA anche DEL PENULTIMO TRADE SE DEVIATION > 0.27 nei x secondi del PENULTIMO TRADE ( qualche volta IL BUY)
-            #if ((seconds_since_last_trade > 0 and seconds_since_last_trade >= min_buy_delay_in_seconds and deviation > 0.27)
-                #or (seconds_since_last_trade == 0 or seconds_since_last_trade > min_buy_delay_in_seconds)):
+            # riga 131 - riga 65 - e riga 100 COMPRA UN PO' PIU' SOPRA anche DEL PENULTIMO TRADE SE DEVIATION > 0.27 nei x secondi del PENULTIMO TRADE ( qualche volta IL BUY)
+            #if ((seconds_since_prev_trade > 0 and seconds_since_prev_trade >= min_buy_delay_in_seconds and deviation > 0.27)
+                #or (seconds_since_prev_trade == 0 or seconds_since_prev_trade > min_buy_delay_in_seconds)):
                 
                 
                
