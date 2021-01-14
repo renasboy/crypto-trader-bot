08b39842daf2e898e58b089cbea317475ebf8493
DNABM <36816435+DNABM@users.noreply.github.com>
2021-01-14 19:11:07 +0100

    125 e 126 sempre casino (per deviation 2)

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -122,8 +122,8 @@ class ro_cano_che_ritorna(object):
                 or (seconds_since_last_trade == 0 or seconds_since_last_trade > min_buy_delay_in_seconds)):
                 
             # COMPRA UN PO' PIU' SOPRA anche DEL PENULTIMO TRADE SE DEVIATION > 0.27 nei 540 secondi (ci vorrebbe un altro tempo ) dal PENULTIMO TRADE ( qualche volta IL BUY)
-            if ((seconds_since_prev_trade > 0 and seconds_since_prev_trade >= min_buy_delay_in_seconds and deviation > 0.27)
-                or (seconds_since_prev_trade == 0 or seconds_since_prev_trade > min_buy_delay_in_seconds)):
+            #if ((seconds_since_prev_trade > 0 and seconds_since_prev_trade >= min_buy_delay_in_seconds and deviation > 0.27)
+                #or (seconds_since_prev_trade == 0 or seconds_since_prev_trade > min_buy_delay_in_seconds)):
                 
                
             
