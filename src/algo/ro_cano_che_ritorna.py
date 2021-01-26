6e5ec8e3014cfd801835cea83a2893427d647307
DNABM <36816435+DNABM@users.noreply.github.com>
2021-01-26 13:53:53 +0100

    299-300 allineamento

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -311,8 +311,8 @@ class ro_cano_che_ritorna(object):
                     
                     
                  if (seconds_since_last_trade > 1200 and seconds_since_last_trade <= 1800
-                    and deviation < -0.20 and ma2_last < ma15_last and ma43 < ma43_2min_ago):
-                    action = 'sell'    
+                     and deviation < -0.20 and ma2_last < ma15_last and ma43 < ma43_2min_ago):
+                     action = 'sell'    
                     
                     
                     
