4fa36f8b48cb59fc1098a00265189b583c0276cb
DNABM <36816435+DNABM@users.noreply.github.com>
2021-01-26 13:40:20 +0100

    riga 274 -piccola modifica ma

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -271,7 +271,7 @@ class ro_cano_che_ritorna(object):
                 ############################################################################################
                 
                 elif (seconds_since_last_trade > 720 and seconds_since_last_trade <= 1200
-                    and deviation > 0.20 and ma2_last < ma15_last and ma43 > ma43_2min_ago):
+                    and deviation > 0.20 and ma2_last < ma11_last and ma43 > ma43_2min_ago):
                     action = 'sell'     
                 
                 if (seconds_since_last_trade > 720 and seconds_since_last_trade <= 1200
@@ -281,7 +281,7 @@ class ro_cano_che_ritorna(object):
                 
                 
                 if (seconds_since_last_trade > 720 and seconds_since_last_trade <= 1200
-                    and deviation < -0.60 and ma2_last < ma15_last and ma43 > ma43_2min_ago):
+                    and deviation < -0.60 and ma2_last < ma11_last and ma43 > ma43_2min_ago):
                     action = 'sell' 
                 
                 
