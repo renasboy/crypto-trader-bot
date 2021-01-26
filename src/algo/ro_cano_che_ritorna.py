a9765c1456294b3741c72406c8764b05f3b1262f
DNABM <36816435+DNABM@users.noreply.github.com>
2021-01-26 13:26:14 +0100

    # VENDITA 1 - da 5 a 12 minuti

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -247,15 +247,22 @@ class ro_cano_che_ritorna(object):
                 ########################################################################################
                 
                 elif (seconds_since_last_trade > 300 and seconds_since_last_trade <= 720
-                      and deviation < 0.20 and ma2_last < ma7_last):
+                      and deviation > 0.10 and ma2_last < ma7_last and ma43 > ma43_2min_ago):
                       action = 'sell'        
                 
                 
                 if (seconds_since_last_trade > 300 and seconds_since_last_trade <= 720
-                    and deviation < -0.29 and ma2_last < ma11_last):
+                    and deviation > 0.03 and ma2_last < ma7_last and ma43 < ma43_2min_ago):
+                    action = 'sell'  
+                
+                
+                if (seconds_since_last_trade > 300 and seconds_since_last_trade <= 720
+                    and deviation < -0.60 and ma2_last < ma11_last and ma43 > ma43_2min_ago):
                     action = 'sell' 
                 
-                
+                if (seconds_since_last_trade > 300 and seconds_since_last_trade <= 720
+                    and deviation < -0.50 and ma2_last < ma11_last and ma43 < ma43_2min_ago):
+                    action = 'sell'
                 
                 
                 ############################################################################################
