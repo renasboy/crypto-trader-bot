5b2bf55b87f2511e00f9e8960f10291266464150
DNABM <36816435+DNABM@users.noreply.github.com>
2021-02-04 20:00:16 +0100

    if - elif

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -633,14 +633,21 @@ class ro_cano_che_ritorna:
 
             # 1) (stop loss) (salvagente)  VENDE SUBITO ( se la perdita e' troppa and incrocio al ribasso ma2 - ma20 )
             
-            if deviation < -1.00 and ma2_last < ma20_last and ma100_last > ma100_13min_ago:
-            
+            if (
+                deviation < -1.00 
+                and ma2_last < ma20_last 
+                and ma100_last > ma100_13min_ago:
+            ):
                 action = "sell"
             
             
-            elif deviation < -1.20 and ma2_last < ma20_last and ma100_last < ma100_13min_ago:
-                
+            elif (
+                deviation < -1.20 
+                and ma2_last < ma20_last 
+                and ma100_last < ma100_13min_ago:
+            ):    
                 action = "sell"
+            
             
             
             # 2) ro cano VENDE SE DIMINUISCE LA FORZA ! ( vende se perdita  < -0.50 e se etc.)
