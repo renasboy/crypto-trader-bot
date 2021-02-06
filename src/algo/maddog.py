34bed4ef0ae3252c5bae3d2a9a5153b11cf56846
DNABM <36816435+DNABM@users.noreply.github.com>
2021-02-06 19:06:32 +0100

    232 vende

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -224,13 +224,14 @@ class maddog:
 
             # VENDE sessione 1
             if self.session == 1:
-                if ma2_last < ma11_last:
+                if ma2_last < ma16_last
+                and deviation > 0.12 or deviation < -0.80:
                     
                         action = "sell"
 
             # VENDE sessione 2
             elif self.session == 2:
-                if ma2_last < ma13_last:
+                if ma2_last < ma16_last:
                     
                         action = "sell"
 
