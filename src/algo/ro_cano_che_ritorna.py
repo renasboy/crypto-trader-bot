1da5209c2fbf976146805ae8c00870052b0b9c0f
DNABM <36816435+DNABM@users.noreply.github.com>
2021-02-12 15:34:53 +0100

    BUY 3 in poi piu' veloce se ma100 up

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -310,7 +310,7 @@ class ro_cano_che_ritorna:
                         and ma2_last > ma7_last
                         and ma2_last > ma2_2_min_ago
                         and ma4_last > ma4_2_min_ago
-                        and ma7_last >= ma15_last
+                        #and ma7_last >= ma15_last
                         #and ma10_last >= ma10_2_min_ago
                         #and ma11_last > ma15_last
                         
