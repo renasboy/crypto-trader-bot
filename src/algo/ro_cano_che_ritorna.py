14247291df9f0966e3a6c59e3f100d71b0a805fa
DNABM <36816435+DNABM@users.noreply.github.com>
2021-02-24 08:20:27 +0100

    deviation_ma 0.60 se tutto scende

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -239,7 +239,7 @@ class ro_cano_che_ritorna:
                         
                           #and deviation > 0.14
                           #and deviation_prev > 0.14
-                          and deviation_ma >= 0.58
+                          and deviation_ma >= 0.60
                           
                           
                         
@@ -275,7 +275,7 @@ class ro_cano_che_ritorna:
                         
                           #and deviation > 0.14
                           #and deviation_prev > 0.14
-                          and deviation_ma >= 0.58
+                          and deviation_ma >= 0.60
                           
                      
                           
@@ -309,7 +309,7 @@ class ro_cano_che_ritorna:
                         
                           #and deviation > 0.14
                           #and deviation_prev > 0.14
-                          and deviation_ma >= 0.01
+                          and deviation_ma >= 0.25
                           
                           
                         
