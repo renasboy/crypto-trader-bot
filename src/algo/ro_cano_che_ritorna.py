a9d98966dbac5ee8d9d39a4b7c70b7f212b2aeb5
DNABM <36816435+DNABM@users.noreply.github.com>
2021-02-24 19:59:56 +0100

    righe 129 e 130 - la mia domanda al programmatore
    ave compa !

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -126,7 +126,9 @@ class ro_cano_che_ritorna:
 
         # SI APRE LA GABBIA SE
         if ma2_last >= ma4_last:
-            
+        #if ma36_last >= ma36_2_min_ago and ma8_last > ma14_last:
+        #elif ma36_last < ma36_2_min_ago and ma2_last > ma4_last:
+        #
             
             # NON TOCCARE QUESTA CONDIZIONE SERVE PER APERTURA DI GABBIA !
             if not self.session or not self.open:
