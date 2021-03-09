52cbd2427693d9b4d1f2cdde612857a32bcb4d52
DNABM <36816435+DNABM@users.noreply.github.com>
2021-03-09 17:24:33 +0100

    riga 220 - and deviation_ma >= 0.13
    prima era 0.10
    e ha comprato un po' troppo presto

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -217,7 +217,7 @@ class ro_cano_che_ritorna:
                         and ma36_last >= ma36_2_min_ago
                         and ma8_last >= ma14_last
                         
-                        and deviation_ma >= 0.10
+                        and deviation_ma >= 0.13
                         
                         and (ma2_prev < ma3_prev and ma2_last > ma3_last) or (price_1_min_ago < ma3_prev and price > ma3_last)
                         # e' un tentativo modesto di mettere incrocio price_prev < ma3_prev and price > ma3_last ( CHIEDI A COMPA, se hai il coraggio )
