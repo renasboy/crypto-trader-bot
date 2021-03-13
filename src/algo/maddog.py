44fdb23bfa99982f9f695b24ae6feb02f6c58516
DNABM <36816435+DNABM@users.noreply.github.com>
2021-03-13 19:17:49 +0100

    AVE COMPA - compra 2 aggiunta deviation_ma 0.10

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -183,16 +183,19 @@ class maddog:
                         #piu' veloce !
                         #and (ma2_prev < ma4_prev and ma2_last > ma4_last)
                         
-                        price > price_1_min_ago
-                        and price > price_2_min_ago
+                        
+                        price > price_2_min_ago
                         and price > price_3_min_ago
+                        and deviation_ma > 0.10
+                        
+
                         #deviation > 0.10 dopo che ha venduto ha comprato nello stesso minuto (puntino verde proprio sopra puntino rosso) con il price che scendeva
                         #allora tolgo la deviation e lascio solo price 1 min ago e price 2 min ago per vedere se funziona ! - FUNZIONA ! vai compaaaaaaaaaaaa
                         
                         #BUY 2 SARA' COSI'
                         #deviation > 0.10 and ma2_prev < ma4_prev and ma2_last > ma4_last
 
-                        #and deviation_ma > 0.10
+                        
 
                         
                        
@@ -279,7 +282,7 @@ class maddog:
                
                 if (
                     ma100_last > ma100_13_min_ago
-                    and ma3_last < ma15_last and deviation > 0.35
+                    and ma2_last < ma15_last and deviation > 0.25
                     
                 ):
                    
@@ -305,7 +308,7 @@ class maddog:
                 
                 if (
                     ma100_last > ma100_13_min_ago
-                    and ma3_last < ma15_last and deviation > 0.35
+                    and ma2_last < ma15_last and deviation > 0.30
                     
                 ):
                    
