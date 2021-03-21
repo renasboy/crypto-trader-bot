17f19aa5fb98d58ff2683cdd64faf2d04b7a44b0
DNABM <36816435+DNABM@users.noreply.github.com>
2021-03-21 18:55:42 +0100

    deviation_buy1 = (ma8_last / ma78_last - 1) * 100
    torniamo

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -89,11 +89,11 @@ class maddog:
     
     
         ######################################################################################################################################################    
-    
+        ###################################################################################################################################################### 
         
 
         # formula "deviation_buy1" (per comprare LA PRIMA VOLTA durante il TREND RIBASSISTA)
-        deviation_buy1 = (ma78_last / ma8_last - 1) * 100 if ma8_last else 0
+        deviation_buy1 = (ma8_last / ma78_last - 1) * 100 if ma78_last else 0
         self.algo_helper.log("deviation_buy1: {}".format(deviation_buy1))
         
         
@@ -117,10 +117,10 @@ class maddog:
         #if ma2_last > ma38_last:
       
         #ma fai cosi' (che la gabbia resterà aperta come se fosse ma78 > ma78 2 min ago !)
-        if ma2_last > ma38_last and deviation_buy1 < 0.20:
+        if ma2_last > ma38_last and deviation_buy1 > -0.40:
             
         #if ma2_last > ma38_last and deviation_buy1 > -0.36: vedi questo valore alle ore 9:43
-        # TI RICORDO CHE LA deviation_buy1 prende ma7_last / ma39_last (per es. (47799 / 47966) -1) * 100 = -0.35 !
+        # TI RICORDO CHE LA deviation_buy1 prende (ma8_last / ma78_last - 1) * 100
             
             
             
@@ -171,7 +171,7 @@ class maddog:
                         
                         #potrai provare- DICO PROVARE- anche un incrocio anticlassico  ma4<ma38 and ma4>ma50
                         
-                        deviation_buy1 < 0.14
+                        deviation_buy1 > -0.30
                         and price > price_1_min_ago
                         and price > price_2_min_ago
                         
@@ -215,7 +215,7 @@ class maddog:
                         price > price_2_min_ago
                         and ma2_last > ma2_2_min_ago
                         
-                        and deviation_buy1 < 0.15
+                        and deviation_buy1 > 0.10
                         and deviation > 0.12
                         
                         
@@ -238,7 +238,7 @@ class maddog:
                     if (
                         
                         ma2_last >= ma4_last
-                        and deviation_buy1 < 0.15 or (ma3_prev < ma8_prev and ma3_last > ma8_last and ma12_last > ma12_2_min_ago)
+                        and deviation_buy1 > 0.10 or (ma3_prev < ma8_prev and ma3_last > ma8_last and ma12_last > ma12_2_min_ago)
                         and deviation > 0.10
                         and price > price_2_min_ago
 
