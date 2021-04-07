20a51f5b792bebec901a7052a25be5d2ac27a0ee
DNABM <36816435+DNABM@users.noreply.github.com>
2021-04-07 19:01:56 +0200

    and ma8_prev < ma50_prev and ma8_last > ma78_last

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -290,7 +290,7 @@ class ro_cano_che_ritorna:
                         
                         price > price_2_min_ago
                         and price > price_3_min_ago
-                        and ma8_prev < ma50_prev and ma6_last > ma78_last or deviation_buy1 > 0.20
+                        and ma8_prev < ma50_prev and ma8_last > ma78_last or deviation_buy1 > 0.20
                         
                         #roma 2 aprile 2021 - BUY 1
                         
@@ -412,7 +412,7 @@ class ro_cano_che_ritorna:
                           
                           price > price_2_min_ago
                           and price > price_3_min_ago
-                          and ma8_prev < ma50_prev and ma6_last > ma78_last or deviation_buy1 > 0.20
+                          and ma8_prev < ma50_prev and ma8_last > ma78_last or deviation_buy1 > 0.20
                         
                           #roma 2 aprile 2021 - BUY 1
                           
