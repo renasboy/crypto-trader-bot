561ffe6dbde5cf63c666b6bb194a68bb7b51e834
DNABM <36816435+DNABM@users.noreply.github.com>
2021-04-11 19:03:16 +0200

    39-78

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -216,7 +216,7 @@ class ro_cano_che_ritorna:
                         price > price_2_min_ago
                         and price > price_3_min_ago
                         and ma2_last > ma2_2_min_ago
-                        and ma50_prev < ma78_prev and ma50_last > ma78_last or deviation_buy1 > 0.50
+                        and ma39_prev < ma78_prev and ma39_last > ma78_last or deviation_buy1 > 0.50
                         
                         #roma 2 aprile 2021 - BUY 1
           
