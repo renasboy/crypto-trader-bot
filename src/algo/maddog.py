96ee2d02f85681621c0482b38ee60653bdf177e1
DNABM <36816435+DNABM@users.noreply.github.com>
2021-04-21 11:04:53 +0200

    vai combaaaaa

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -219,10 +219,10 @@ class maddog:
                         and ma3_last > ma40_last
                         #incredibile ma vero E' NECESSARIA quando deve ricomprare dopo la correzione al ribasso
                         
-                        and deviation > 0.165
+                        and deviation > 0.16
                         #deviation = ma2_last / last_trade_price
                         
-                        and deviation_buy2 > 0.125
+                        and deviation_buy2 > 0.12
                         #deviation_buy2 = ma8_last / ma78_last
                    
                     ):
