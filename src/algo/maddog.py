afd515e051653713f62daf4534960abb7d6eeedc
DNABM <36816435+DNABM@users.noreply.github.com>
2021-04-22 19:30:25 +0200

    vende 1 con ma13 < ma50 !

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -278,7 +278,7 @@ class maddog:
                 
                 if (
                     
-                    ma3_last < ma43_last and deviation > 0.12 or ( ma3_last < ma20_last and deviation > 2.49 )
+                    ma13_last < ma50_last and deviation > 0.12 or ( ma3_last < ma20_last and deviation > 2.49 )
                     
                 ):
                    
