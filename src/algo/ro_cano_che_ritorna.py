1c3ce070058f5aacfd30ea8fad4dc2c34891a539
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-06 20:54:42 +0200

    0.18 min gain

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -782,7 +782,7 @@ class ro_cano_che_ritorna:
                 
                 if (
                    
-                    ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.12 ) or ( ma2_last < ma13_last and deviation_sell > 0.80 ) or (( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 )
+                    ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.18 ) or ( ma2_last < ma13_last and deviation_sell > 0.80 ) or (( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 )
                     #deviation_sell = ma2_last / last_trade_price
                     
                     
@@ -860,7 +860,7 @@ class ro_cano_che_ritorna:
                 if (
                     
                     
-                    ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.12 ) or ( ma2_last < ma13_last and deviation_sell > 0.80 ) or (( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 )
+                    ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.18 ) or ( ma2_last < ma13_last and deviation_sell > 0.80 ) or (( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 )
                     #deviation_sell = ma2_last / last_trade_price
                     
                     
