7aa5f2571e4d7ab2bc7a30793525b3f701340f0f
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-12 07:02:26 +0200

    Sell dovrebbe essere 2 2-39 o 3-33
    C'e' ancora ma78

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -363,7 +363,7 @@ class maddog:
                 elif (
                     
                     ma2_last < ma2_2_min_ago
-                    and ( ma2_last < ma39_last and deviation_sell < -0.45 ) or ( ma2_last < ma39_last and ( ma2_last < ma50_last and deviation_ma50 < -0.45 ))
+                    and ( ma2_last < ma39_last and deviation_sell < -0.01 ) or ( ma3_last < ma33_last and and deviation_ma50 < -0.01 )
                     #deviation_sell = ma2_last / ma78_last
                     #deviation_buy1 = (ma8_last / ma78_last
                     
