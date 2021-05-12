d92a4511aaed2d289c360e75daecf43d94623ca2
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-12 10:08:45 +0200

    allineamento

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -365,8 +365,9 @@ class maddog:
                     
                     ma2_last < ma2_2_min_ago
                     and ( ma2_last < ma39_last and deviation_sell < -0.01 ) or ( ma3_last < ma33_last and and deviation_ma50 < -0.01 )
+                    
                     #deviation_sell = ma2_last / ma78_last
-                    #deviation_buy1 = (ma8_last / ma78_last
+                    #deviation_buy1 = ma8_last / ma78_last
                     
                     
                 ):
