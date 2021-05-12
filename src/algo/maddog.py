60f89709a80444627f48c2f3defd2205adbf087c
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-12 10:21:01 +0200

    riga 371 non riesco ad allineare

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -449,5 +449,5 @@ class maddog:
         # grazie compa ### #
         #
         # plinius  ##################
-        # comparo de dio ###### ##########
+        # comparo de dio ###### ###########
         
