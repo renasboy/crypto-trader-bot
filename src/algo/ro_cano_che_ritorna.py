cbae44c15d4cd3e999be516b3d4891dec7ab640c
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-14 11:02:35 +0200

    993 )):

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -990,7 +990,7 @@ class ro_cano_che_ritorna:
                     and ( ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.18 )) or ( ma50_last >= ma50_2_min_ago and ( ma2_last < ma13_last and deviation_sell > 0.60 )) or ( ma50_last >= ma50_2_min_ago and ( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 ) or ( ma50_last >= ma50_2_min_ago and ( ma3_last > ma50_last and ma3_last < ma50_last )
                     #deviation_sell = ma2_last / last_trade_price
                     
-                ):
+                )):
                   
                     sell = "SELL #25"
                     action = "sell"
