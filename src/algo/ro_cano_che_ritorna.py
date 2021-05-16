769d49c6ac76cdb5849c01d4825225b979147319
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-16 20:25:00 +0200

    prova 8-50 buy 1

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -271,9 +271,10 @@ class ro_cano_che_ritorna:
                         
                         ma78_last > ma78_40_min_ago
                       
-                        and (deviation_buy1 > 0.05 and (ma13_prev < ma78_prev and ma13_last > ma78_last)) or (deviation_buy1 > 0.10 )
-                        and (deviation_buy1 > 0.05 and (ma18_prev < ma78_prev and ma18_last > ma78_last)) or (deviation_buy1 > 0.10 )
+                        #and (deviation_buy1 > 0.05 and (ma13_prev < ma78_prev and ma13_last > ma78_last)) or (deviation_buy1 > 0.10 )
+                        #and (deviation_buy1 > 0.05 and (ma18_prev < ma78_prev and ma18_last > ma78_last)) or (deviation_buy1 > 0.10 )
                         
+                        and (deviation_buy1 > 0.05 and (ma8_prev < ma50_prev and ma8_last > ma50_last)) or (deviation_buy1 > 0.10 )
                         
                         and price > price_2_min_ago
                         and ma2_last > ma2_2_min_ago
@@ -301,7 +302,9 @@ class ro_cano_che_ritorna:
                     elif (
                         
                         ma78_last < ma78_20_min_ago
-                        and (deviation_buy1 > 0.65 and (ma25_prev < ma78_prev and ma25_last > ma78_last)) or (deviation_buy1 > 0.85 )
+                        #and (deviation_buy1 > 0.65 and (ma25_prev < ma78_prev and ma25_last > ma78_last)) or (deviation_buy1 > 0.85 )
+                        
+                        and (deviation_buy1 > 0.65 and (ma8_prev < ma50_prev and ma8_last > ma50_last)) or (deviation_buy1 > 0.85 )
                         
                         and ma8_last > ma50_last
                         
