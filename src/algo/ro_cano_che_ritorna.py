79e12e76f3d0293260c250d9d10db036dc07e29c
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-17 18:23:14 +0200

    buy 1 mentre scende

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -304,7 +304,10 @@ class ro_cano_che_ritorna:
                         ma78_last < ma78_20_min_ago
                         #and (deviation_buy1 > 0.65 and (ma25_prev < ma78_prev and ma25_last > ma78_last)) or (deviation_buy1 > 0.85 )
                         
-                        and (deviation_buy1 > 0.85 and (ma18_prev < ma78_prev and ma18_last > ma78_last)) or (deviation_buy1 > 0.95 )
+                        and (deviation_buy1 > 0.90 and (ma18_prev < ma78_prev and ma18_last > ma78_last)) or (deviation_buy1 > 0.99 )
+                        
+                        #and ma39_last > ma50_last
+                        #questa sta nel maddog che va molto bene durante i ribassi
                         
                         and ma8_last > ma50_last
                         
