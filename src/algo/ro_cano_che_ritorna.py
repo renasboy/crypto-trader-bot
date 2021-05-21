8d26666d74179cbc4123780f2767452d9755fb72
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-21 09:19:15 +0200

    x and y DEVI METTERE COSI' (X and Y) !
    ma50_last>=ma50_2_min_ago and deviation_sell<-0.45 
    mettere le parenti all' inizio e alla fine

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -617,7 +617,7 @@ class ro_cano_che_ritorna:
                 elif (
                     
                     #ma2_last < ma2_2_min_ago
-                    ma50_last >= ma50_2_min_ago and deviation_sell < -0.45
+                    ( ma50_last >= ma50_2_min_ago and deviation_sell < -0.45 )
                     #deviation_sell = ma2_last / last_trade_price
                  
                 ):
