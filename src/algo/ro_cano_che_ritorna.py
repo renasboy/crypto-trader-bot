ce047a640c976091732892f1ccf4218939a7b637
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-29 11:49:18 +0200

    piccolo errore

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -878,7 +878,7 @@ class ro_cano_che_ritorna:
                     ( ma50_last >= ma50_2_min_ago and ( ma3_last > ma30_last and ma3_last < ma30_last ) and deviation_sell > 0.18 )
                     #deviation_sell = ma2_last / last_trade_price
                    
-                )):
+                ):
                    
                     sell = " SELL #1 (0-3 min) con ma50 > "
                     action = "sell"
