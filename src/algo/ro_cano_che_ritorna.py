6cce88da38938234b822089a5148f209490b612b
DNABM <36816435+DNABM@users.noreply.github.com>
2021-05-29 12:05:24 +0200

    divisione or

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -1083,19 +1083,48 @@ class ro_cano_che_ritorna:
 
                 
                 if (
-                    
-                    #ma2_last < ma2_2_min_ago
-                    ( ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.18 )) or ( ma50_last >= ma50_2_min_ago and ( ma2_last < ma18_last and deviation_sell > 0.80 )) or ( ma50_last >= ma50_2_min_ago and ( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 ) or ( ma50_last >= ma50_2_min_ago and ( ma3_last > ma50_last and ma3_last < ma50_last )
+                   
+                    ( ma50_last >= ma50_2_min_ago and ( ma3_last < ma25_last and deviation_sell > 0.18 )
                     
                     #deviation_sell = ma2_last / last_trade_price
-                    
-                    #and ma13_last > ma13_2_min_ago
-
-                    
-                )):
+                
+                ):
                     sell = " SELL #9 (3-5 min) con ma50 > "
                     action = "sell"
+                    
+                    
+                    
+                    
+                elif (
+                   
+                    ( ma50_last >= ma50_2_min_ago and ( ma2_last < ma18_last and deviation_sell > 0.80 )
+                    
+                    #deviation_sell = ma2_last / last_trade_price
+                
+                ):
+                    sell = " SELL #9 (3-5 min) con ma50 > "
+                    action = "sell"
+                    
+                    
+                    
+                    
+                elif (
+                   
+                    ( ma50_last >= ma50_2_min_ago and ( ma3_last > ma78_last and ma3_last < ma78_last ) and deviation_sell < -0.20 )
+                    
+                    #deviation_sell = ma2_last / last_trade_price
+                
+                ):
+                    sell = " SELL #9 (3-5 min) con ma50 > "
+                    action = "sell"
+                    
+                    
+                    
+                    
+               
 
+                
+             ##################################################################################################################   
                 
                 
                 
