f4ea5e018476f1241da280eb53effbd0384e26c2
DNABM <36816435+DNABM@users.noreply.github.com>
2021-10-03 18:53:57 +0200

    Buy2 tolto and incrocio classico
    Potrebbe non verificarsi mentre sale con una certa velocita'

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -363,10 +363,10 @@ class ro_cano_che_ritorna:
               
                 if (
                     deviation_buy_ma3_sopra_ma13 > 0.10
-                    and deviation_buy2 > 0.18
-                    and deviation_ma7_sopra_ma40 > 0.12
+                    and deviation_buy2 > 0.10
+                    and deviation_ma7_sopra_ma40 > 0.10
                     
-                    and (ma4_prev < ma9_prev and ma4_last > ma9_last) 
+                    #and (ma4_prev < ma9_prev and ma4_last > ma9_last) 
                     
                     and ma2_last > ma2_2_min_ago
                     and price > price_2_min_ago
