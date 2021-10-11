c7a6622284c5af0cd3b660a8d83cee0b0a605741
DNABM <36816435+DNABM@users.noreply.github.com>
2021-10-11 13:24:08 +0200

    nulla

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -574,7 +574,7 @@ class ro_cano_che_ritorna:
             # VENDITA - da 0 a 3 minuti = da 0 a 180 secondi
             if seconds_since_last_trade > 0 and seconds_since_last_trade <= 180:
                 
-                con ma50 >
+                # con ma50 >
                 
                 if (
                     ma50_last > ma50_2_min_ago 
