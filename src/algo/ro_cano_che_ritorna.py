12d91e63fc43ba7081efeda5cc1676249ba9ff0f
DNABM <36816435+DNABM@users.noreply.github.com>
2021-10-12 08:14:16 +0200

    Sell tempo piu' giu' cosi' non vende continuamente

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -1124,11 +1124,11 @@ class ro_cano_che_ritorna:
             elif (
                 seconds_since_last_trade > max_hold_time_in_seconds 
                 and ma8_last < ma39_last
-                and deviation_sell < -0.33
+                and deviation_sell < -0.35
               
             ):
 
-                sell = "SELL TEMPO 2 riga 1128"
+                sell = "SELL TEMPO 2 riga 1131"
                 action = "sell"
 
                 
