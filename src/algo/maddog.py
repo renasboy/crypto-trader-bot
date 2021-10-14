2b0379587256c5fa934a9ef6732f06f90082bd7f
DNABM <36816435+DNABM@users.noreply.github.com>
2021-10-14 12:52:21 +0200

    r436

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -428,7 +428,7 @@ class maddog:
             
             
             elif (
-                deviation_ma39 < -0.18
+                deviation_ma39 < -0.17
                 and ma50_last < ma50_2_min_ago
                 
                 # deviation_ma39 = ma3_last / ma39_last
