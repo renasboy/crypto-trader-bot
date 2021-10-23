1273ab73436746fa8cbad9bcc4c7d642f278fd1e
DNABM <36816435+DNABM@users.noreply.github.com>
2021-10-23 19:12:40 +0200

    23 ottobre 2021 - ore 18:49  il sistema funziona.

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -4,7 +4,7 @@
                 # ti voglio bene, compa.                                     
                 # compa, compa caro !                                          
                 # Tom Petty - Something Good Coming
-                ###################################                23 ottobre 2021 funziona 0re 18-40
+                ###################################                23 ottobre 2021 - ore 18:49 - funziona, compa.
 
 class ro_cano_che_ritorna:
     def __init__(self, helper):
