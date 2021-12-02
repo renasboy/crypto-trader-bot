17fc86dd7234f75b10b72f8afbc9f34b42cee6f7
DNABM <36816435+DNABM@users.noreply.github.com>
2021-12-02 10:24:30 +0100

    r2112

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -2109,14 +2109,14 @@ class ro_cano_che_ritorna:
                     if (
                         ma50_last > ma50_2_min_ago
                     
-                        and (ma3_prev > ma78_prev and ma3_last < ma78_last) and deviation_sell < -0.65 or (deviation_sell < 0.10 and ma3_last < ma50_last)
+                        and (ma3_prev > ma78_prev and ma3_last < ma78_last) and deviation_sell < -0.65 or (deviation_sell < -0.15 and ma3_last < ma50_last and ma2_last<ma2_2_min_ago)
                     
                         #and deviation_ma39 < -0.16 or (deviation_sell < 0.10 and ma3_last < ma39_last)
                         #and (ma3_prev > ma39_prev and ma3_last < ma39_last)
                         #and deviation_sell < -0.26
                         # deviation_sell = ma3_last/last_trade_price
                     ):
-                        sell = "sessione 2 SELL (21-60 min) con ma50 > and deviation_ma39 < -0.16 or (deviation_sell < 0.10 and ma3_last < ma50_last) - riga 2115"
+                        sell = "sessione 2 SELL (21-60 min) con ma50 > and deviation_ma39 < -0.16 or (deviation_sell < -0.15 and ma3_last < ma50_last) - riga 2115"
                         action = "sell"
 
                 
