bfa85daf0c3e5d0b3b63beb27da92508acdcad3f
DNABM <36816435+DNABM@users.noreply.github.com>
2022-01-13 10:24:18 +0100

    BUY 3 arrivava un pochino tardi - ridotta la 4-30

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -481,7 +481,7 @@ class ro_cano_che_ritorna:
                     and ma39_last > ma78_last
                     and deviation_buy3 > 0.02
                     and delta_buy3_incrocio_ma3_ma8 > 0.06
-                    and deviation_ma4_sopra_ma30 > 0.17
+                    and deviation_ma4_sopra_ma30 > 0.15
                     and ma3_last > ma8_last
                     and ma3_last > ma78_last
                     and ma4_last > ma4_2_min_ago
@@ -499,7 +499,7 @@ class ro_cano_che_ritorna:
                     and deviation_buy3 > 0.03
                     and deviation_ma13_sopra_ma25 > 0.045
                     and delta_buy3_incrocio_ma3_ma8 > 0.07
-                    and deviation_ma4_sopra_ma30 > 0.18
+                    and deviation_ma4_sopra_ma30 > 0.15
                     and ma3_last > ma8_last
                     and ma3_last > ma78_last
                     and ma4_last > ma4_2_min_ago
