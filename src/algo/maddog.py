12904864049124f7475da31d871e7e6974844177
DNABM <36816435+DNABM@users.noreply.github.com>
2022-02-03 09:56:32 +0100

    r1193 VENDITA IN BASSO - VENDITA IN ALTO

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1264,20 +1264,35 @@ class maddog:
                 elif (
                     seconds_since_last_trade > 1261 and seconds_since_last_trade <= 3600
                 ):
-
                     if (
                         ma50_last > ma50_2_min_ago
                         and (ma3_prev > ma78_prev and ma3_last < ma78_last)
                         and deviation_sell < -0.65
                         or (deviation_sell < -0.21 and ma4_last < ma50_last)
                         and ma2_last < ma2_2_min_ago
-                        # and (ma3_prev > ma78_prev and ma3_last < ma78_last) and deviation_sell < -0.65 or (deviation_sell < 0.10 and ma3_last < ma50_last)
+                        # VENDITA IN BASSO 
                         # and deviation_ma39 < -0.16 or (deviation_sell < 0.10 and ma3_last < ma39_last)
                         # and (ma3_prev > ma39_prev and ma3_last < ma39_last)
                         # and deviation_sell < -0.26
                         # deviation_sell = ma3_last/last_trade_price
                     ):
-                        sell = "sessione 1 SELL (21-60 min) con ma50> and incrocio 3-78 and deviation_sell<-0.65 or (deviation_sell <-0.21 and ma4_last < ma50_last) - riga 1193"
+                        sell = "sessione 1 SELL (21-60 min) con ma50> and incrocio 3-78 and deviation_sell<-0.65 or (deviation_sell <-0.21 and ma4_last<ma50_last) - riga 1193"
+                        action = "sell"
+                        
+                        
+                    elif (
+                        ma50_last > ma50_2_min_ago
+                        and (ma3_prev > ma78_prev and ma3_last < ma78_last)
+                        and deviation_sell < -0.65
+                        or (deviation_sell < -0.28 and ma3_last < ma50_last)
+                        and ma2_last < ma2_2_min_ago
+                        # VENDITA 1 IN ALTO dopo BUY IN RISALITA
+                        # and deviation_ma39 < -0.16 or (deviation_sell < 0.10 and ma3_last < ma39_last)
+                        # and (ma3_prev > ma39_prev and ma3_last < ma39_last)
+                        # and deviation_sell < -0.26
+                        # deviation_sell = ma3_last/last_trade_price
+                    ):
+                        sell = "sessione 1 SELL (21-60 min) con ma50> and incrocio 3-78 and deviation_sell<-0.65 or (deviation_sell <-0.28 and ma3_last<ma50_last) - riga 1194"
                         action = "sell"
 
                     elif (
