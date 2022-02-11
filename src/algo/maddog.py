7ec6ba8e2b90234d45f62158e6afeba7e42058c4
DNABM <36816435+DNABM@users.noreply.github.com>
2022-02-11 13:19:36 +0100

    r1193

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1418,15 +1418,16 @@ class maddog:
                         ma50_last > ma50_2_min_ago
                         and (ma3_prev > ma78_prev and ma3_last < ma78_last)
                         and deviation_sell < -0.65
-                        or (deviation_sell < -0.22 and ma5_last < ma50_last)
+                        or (deviation_sell < -0.23 and ma5_last < ma50_last)
                         and ma2_last < ma2_2_min_ago
+                        
                         # VENDITA IN BASSO 
                         # and deviation_ma39 < -0.16 or (deviation_sell < 0.10 and ma3_last < ma39_last)
                         # and (ma3_prev > ma39_prev and ma3_last < ma39_last)
                         # and deviation_sell < -0.26
                         # deviation_sell = ma3_last/last_trade_price
                     ):
-                        sell = "sessione 1 SELL (21-60 min) con ma50> and incrocio 3-78 and deviation_sell<-0.65 or (deviation_sell <-0.22 and ma5_last<ma50_last) - riga 1193"
+                        sell = "sessione 1 SELL (21-60 min) con ma50> and incrocio 3-78 and deviation_sell<-0.65 or (deviation_sell <-0.23 and ma5_last<ma50_last) - riga 1193"
                         action = "sell"
                         
                         
