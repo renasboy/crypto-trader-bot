12383b7fbffce42db395ed94d4ce88d8c769fc53
DNABM <36816435+DNABM@users.noreply.github.com>
2022-02-15 17:59:26 +0100

    r841

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -838,10 +838,10 @@ class maddog:
                     and ma2_last > ma2_2_min_ago
                     and deviation_assurda > -0.10
                     and deviation_ma4_sopra_ma30 > 0.11
+                    and deviation_ma100_laterale > 0.10
                     
-                    # and deviation_ma100_laterale > 0.50
                 ):
-                    buy = "BUY 2 con DEVIATION ASSURDA se ma200 sale da 20 min compra con price - ma200 (sul BUY 2 lo 0.50 evitera' GLI EFFETTI LATERALI !) - riga 497"
+                    buy = "BUY 2 con DEVIATION ASSURDA se ma200 sale da 20 min BUY con PRICE-ma200 (sul BUY 2 lo 0.50 evita molto meglio GLI EFFETTI LATERALI !) - riga 497"
                     action = "buy"
                     percentage = 20
                     
