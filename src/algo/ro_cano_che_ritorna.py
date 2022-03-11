879389323a6bb021cf4e18b160e0f8e538960396
DNABM <36816435+DNABM@users.noreply.github.com>
2022-03-11 20:19:28 +0100

    r624

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -621,12 +621,12 @@ class ro_cano_che_ritorna:
                     and ma200_last > ma200_120_min_ago
                     and deviation_buy_crollo_1 < -0.29
                     and deviation_buy_crollo_1 > -0.59
-                    and ma2_last > ma5_last
+                    and ma2_last > ma6_last
                     
                   
                 ):
 
-                    buy = "BUY 1A con ma200> DURANTE UNA piccola CORREZIONE che non e' un forte ribasso e non e' un crollo - riga 830"
+                    buy = "BUY 1A con ma200> DURANTE UNA piccola CORREZIONE - condizione ferrari - riga 830"
                     action = "buy"
                     percentage = 10
 
