a9ad2f4de2d32dfe3079026ae8c7ea434a246454
DNABM <36816435+DNABM@users.noreply.github.com>
2022-03-17 11:08:28 +0100

    bug se trova 2 condizioni SELL = (session diverse)
    vende con tutte e 2 le sessioni

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -3806,9 +3806,9 @@ class maddog:
                     
                     
                     elif (    
-                        ma50_last < ma50_2_min_ago and (ma2_last < ma2_2_min_ago and deviation_sell < -0.26)
+                        ma50_last < ma50_2_min_ago and (ma2_last < ma2_2_min_ago and deviation_sell < -0.27)
                     ):
-                        sell = "sessione 2 SELL (21-60 min) con ma50 < and deviation_sell < -0.26 - riga 3813"
+                        sell = "sessione 2 SELL (21-60 min) con ma50 < and deviation_sell < -0.27 - riga 3813"
                         action = "sell"
                         
                        
