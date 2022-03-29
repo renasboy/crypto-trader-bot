50917bd266a09398736674e666342ad4b4185ce0
DNABM <36816435+DNABM@users.noreply.github.com>
2022-03-29 17:48:57 +0200

    il BUY 3 deve avere and deviation > -0.30
     altrimenti riparte il BUY  1

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -403,7 +403,7 @@ class ro_cano_che_ritorna:
                     
                     and ma13_last > ma78_last
                     and deviation_bellissima > 0.17
-                    and deviation < -0.30
+                    
                     
                     and ma5_last > ma5_2_min_ago
                     and deviation_ma3_sopra_ma7 > 0.05
@@ -425,7 +425,7 @@ class ro_cano_che_ritorna:
 
                 elif (
                     ma13_last > ma69_last
-                    and deviation < -0.30
+                    
                     
                     and ma72_last >= ma72_2_min_ago
                     and deviation_bellissima > 0.17
@@ -447,7 +447,7 @@ class ro_cano_che_ritorna:
                 
                 elif (
                     deviation_buy1 > 0.25
-                    and deviation < -0.30
+                    
                     
                     and ma13_last > ma50_last
                     and ma78_last > ma78_2_min_ago
@@ -541,7 +541,7 @@ class ro_cano_che_ritorna:
                 
                 elif (
                     ma78_last < ma78_2_min_ago
-                    and deviation < -0.30
+                    
                     and ma39_last > ma78_last
                     and deviation_buy1 > 0.14
                     and deviation_bellissima > 0.18
@@ -562,7 +562,7 @@ class ro_cano_che_ritorna:
                 
                 elif (
                     ma78_last < ma78_2_min_ago
-                    and deviation < -0.30
+                    
                     and ma39_last > ma78_last
                     and deviation_bellissima > 0.18
                     and price > price_2_min_ago
@@ -849,6 +849,8 @@ class ro_cano_che_ritorna:
 
                 if (
                     ma69_last >= ma69_2_min_ago
+                    and deviation > -0.30
+                    
                     and deviation_buy3 > 0.12
                     and ma3_last > ma13_last
                     and deviation_ma7_sopra_ma40 > 0.08
@@ -866,6 +868,8 @@ class ro_cano_che_ritorna:
 
                 elif (
                     deviation_buy3 > 0.02
+                    and deviation > -0.30
+                    
                     and ma39_last > ma72_last
                     and deviation_buy3 > 0.02
                     and delta_buy3_incrocio_ma3_ma8 > 0.06
@@ -888,6 +892,8 @@ class ro_cano_che_ritorna:
 
                 elif (
                     ma78_last < ma78_2_min_ago
+                    and deviation > -0.30
+                    
                     and deviation_buy3 > 0.03
                     and deviation_ma13_sopra_ma25 > 0.04
                     and delta_buy3_incrocio_ma3_ma8 > 0.07
