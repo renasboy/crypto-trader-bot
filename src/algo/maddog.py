927f0eabc1f63b88f1f6e6915dab320156d9803c
DNABM <36816435+DNABM@users.noreply.github.com>
2022-04-09 22:49:31 +0200

    r3526

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -3520,10 +3520,10 @@ class maddog:
                         
                 
                     elif (
-                        ma50_last < ma50_2_min_ago and (ma2_last < ma2_2_min_ago and deviation_sell < -0.13 and ma3_last < ma39_last )
+                        ma50_last < ma50_2_min_ago and (ma2_last < ma2_2_min_ago and deviation_sell < -0.15 and ma3_last < ma39_last )
                        
                     ):
-                        sell = "SELL 1 dopo 90 min con ma50 < and (deviation_sell < -0.13 and ma3_last < ma39_last) - r 3526"
+                        sell = "SELL 1 dopo 90 min con ma50 < and (deviation_sell < -0.15 and ma3_last < ma39_last) - r 3526"
                         action = "sell"
                         
                     
