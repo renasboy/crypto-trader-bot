a93f7f367902028ca8e126d6575b48edee5eaf07
DNABM <36816435+DNABM@users.noreply.github.com>
2022-09-11 17:06:48 +0200

    altre piccole modifiche dei giorni scorsi

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1811,8 +1811,8 @@ class maddog:
                     
                     and ma30_last > ma30_40_min_ago
                 
-                    and deviation_ma5_sopra_ma28 > 0.15
-                    and deviation_bellissima > 0.06
+                    and deviation_ma5_sopra_ma28 > 0.13
+                    and deviation_bellissima > 0.05
                     and ma20_last > ma20_2_min_ago
                     and ma5_last > ma5_2_min_ago
                     and ma2_last > ma20_last
@@ -1824,7 +1824,8 @@ class maddog:
                     
                     # 9 giu 2022 15>28 al posto di 50>100 
                     # 9 giu 2022 8>200 al posto di 100>200
-                    
+                    # 4 set 5-28 0.13 da 0.15
+                    # 4 set dev bellissima 0.05 da 0.06
                     
                     
                 # BUY 1 tempo ESTATE PIU' VELOCE 100 > 60 min ago (solo l' estate se lo puo' perettere!) considera il passare del TEMPO ! ma30 > 
@@ -7073,7 +7074,7 @@ class maddog:
                     
                     and ma100_last < ma200_last
                     and ma200_last < ma300_last
-                    and deviation_ma5_sopra_ma28 > 0.05
+                    and deviation_ma5_sopra_ma28 > 0.13
                     and deviation_buy_ma5_sopra_ma20 > 0.01
                     
                     and ma2_last > ma2_2_min_ago
@@ -7082,7 +7083,7 @@ class maddog:
                     action = "buy"
                     percentage = 70
                     
-                    # aggiunta di 5-28 < 0.12
+                    # aggiunta di 5-28 > 0.12
                     
                     
                     
@@ -12367,13 +12368,14 @@ class maddog:
                         
                         and ma300_last < ma300_120_min_ago
                         and ma100_last < ma200_last
-                        and deviation_sell < -0.05
+                        and deviation_sell < -0.07
                         and ma2_last < ma2_2_min_ago
                     ):
                    
                         sell = "SELL 1 > 110 min con 5 < 200 ! FORSE E' NECESSARA SOLO QUESTA 4 ! deviation_sell < -0.05 - r 10655 B"
                         action = "sell"
                         
+                        # 4 set 2022 dev sell -0.07 da -0.05
          
         
             
