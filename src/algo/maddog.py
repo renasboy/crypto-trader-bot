5aa1eb9fcca102be4710f5152138a1ae830de578
DNABM <36816435+DNABM@users.noreply.github.com>
2022-09-14 15:26:29 +0200

    sempre problema 4571 A e B
    in attesa del compare.
    
    ************* Module maddog
    src/algo/maddog.py:14:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:15:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:16:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:17:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:18:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:19:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:20:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    src/algo/maddog.py:21:29: E1101: Instance of 'maddog' has no 'algo_helper' member (no-member)
    etc.

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -5432,8 +5432,8 @@ class maddog:
                 # BUY 1 DURANTE UN RIBASSO AUDI con 5-16 copiata da RCCR CHE E' ANDATA BENISSIMO ! 22 ago 2022
                 
                 elif (
-                    deviation_ma5_sotto_ma300 > -2.50
                     
+                    deviation_ma5_sotto_ma300 > -3.00
                     and deviation_buy_crollo_1 < -0.60
                     and deviation_buy_crollo_1 > -1.60
                      
@@ -5455,8 +5455,8 @@ class maddog:
                 # BUY 1 DURANTE UN RIBASSO AUDI con 5-16 copiata da RCCR CHE E' ANDATA BENISSIMO ! 22 ago 2022
                 
                 elif (
-                    deviation_ma5_sotto_ma300 < -2.50
                     
+                    deviation_ma5_sotto_ma300 < -3.00
                     and deviation_buy_crollo_1 < -0.60
                     and deviation_buy_crollo_1 > -1.60
                     
