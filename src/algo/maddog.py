bc69aadea129b6c38a7f2c7435be104213db63df
DNABM <36816435+DNABM@users.noreply.github.com>
2022-09-28 11:30:26 +0200

    problema

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1418,7 +1418,7 @@ class maddog:
                 
                 elif (     
                     ma300_last > ma300_301_min_ago
-                    and deviation_ma100_laterale > 0.4
+                    and deviation_ma100_laterale > 0.3
                     and ma200_last > ma300_last
                     and ma78_last < ma100_last
                     and ma8_last > ma100_last
@@ -1440,8 +1440,8 @@ class maddog:
                     
                 elif (     
                     ma300_last > ma300_301_min_ago
-                    and deviation_ma100_laterale < 0.4
-                    and deviation_ma100_laterale > -0.4
+                    and deviation_ma100_laterale < 0.3
+                    
                     and ma200_last > ma300_last
                     and ma78_last < ma100_last
                     and ma8_last > ma100_last
@@ -1462,27 +1462,7 @@ class maddog:
                     
                     
                     
-                elif (     
-                    ma300_last > ma300_301_min_ago
-                    and deviation_ma100_laterale < -0.4
-                    
-                    and ma200_last > ma300_last
-                    and ma78_last < ma100_last
-                    and ma8_last > ma100_last
-                    
-                    and ma200_last < ma200_60_min_ago
-                    and ma100_last < ma100_60_min_ago
-                    
-                    and deviation_ma5_sopra_ma28 > 0.15
-                    and deviation_bellissima > 0.11
-                    and ma2_last > ma2_2_min_ago
-                ):     
-                  
-                    buy = "BUY 1 MOLTO RISCHIOSO modo 2 ! quando incomincia il ribasso MA ma300 > E 200>300 con 78<100 e 5-28 > 0.19 - r 1161 A2z"
-                    action = "buy"
-                    percentage = 80
-                    
-                    # 28 set se 300 sale da 5 ore ho anticipato il buy di una ndecchiecella
+                
                     
                     
                     
