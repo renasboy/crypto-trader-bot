bd1d7ea7d9c49ea0a36b44710f43832890544a29
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-01 18:55:26 +0200

    1184 b1x e b1y speriamo

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1482,8 +1482,10 @@ class maddog:
                     
                 # ------------------------------------------------------------ BUY 1 MOLTO RISCHIOSO modo 2 ! quando incomincia il ribasso MA ma300 > E MA 200<300
                 
+                
                 elif (     
                     ma300_last > ma300_120_min_ago
+                    and ma59_last > ma125_last
                     and ma200_last < ma300_last
                     and ma5_last < ma300_last
                     and ma5_last > ma100_last
@@ -1496,7 +1498,29 @@ class maddog:
                     and ma2_last > ma2_2_min_ago
                 ):     
                   
-                    buy = "BUY 1 quando incomincia il ribasso MA ma300> MA 200<300 CON 5 SOTTO 300 ! E COMPRA CON 5 SOPRA 100 ! - r 1184 B1"
+                    buy = "BUY 1 quando incomincia il ribasso MA ma300> MA 200<300 CON 5 SOTTO 300 ! E COMPRA CON 5 SOPRA 100 ! - r 1184 B1x"
+                    action = "buy"
+                    percentage = 90
+                    
+                    # 20 set 2022 aggiunto 3-10 > 0.11
+                    
+                    
+                elif (     
+                    ma300_last > ma300_120_min_ago
+                    and ma59_last < ma125_last
+                    and ma200_last < ma300_last
+                    and ma5_last < ma300_last
+                    and ma5_last > ma100_last
+                    
+                    and deviation_ma3_sopra_ma10 > 0.135
+                    
+                    and ma200_last < ma200_60_min_ago
+                    and ma100_last < ma100_60_min_ago
+              
+                    and ma2_last > ma2_2_min_ago
+                ):     
+                  
+                    buy = "BUY 1 quando incomincia il ribasso MA ma300> MA 200<300 CON 5 SOTTO 300 ! E COMPRA CON 5 SOPRA 100 ! - r 1184 B1y"
                     action = "buy"
                     percentage = 90
                     
