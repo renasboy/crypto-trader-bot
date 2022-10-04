b650433d169a0af1757d8aeb74cb59a83c755d96
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-04 14:45:56 +0200

    16743

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -20042,11 +20042,30 @@ class maddog:
                 
                 
                 
+            # 32 B1a - SELL condizione speciale ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90
+            
+            elif (
+                seconds_since_last_trade > max_hold_time_in_seconds_sell_5_90
+                and deviation_sell > 0.80
+                and ma5_last < ma50_last
+                and deviation_ma5_sotto_ma300 > 0.25
+                and ma300_last > ma300_120_min_ago
+                
+                and ma2_last < ma2_2_min_ago
+           
+            ):
+
+                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-50 - r 16743 B1aX"
+                action = "sell"
+                        
+                # 24 set 2022 ore 17:23
+                
                 
             # 32 B1a - SELL condizione speciale ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90
             
             elif (
                 seconds_since_last_trade > max_hold_time_in_seconds_sell_5_90
+                and deviation_sell < 0.80
                 and deviation_ma5_sotto_ma300 > 0.25
                 and ma300_last > ma300_120_min_ago
                 and ma5_last < ma90_last
@@ -20054,10 +20073,17 @@ class maddog:
            
             ):
 
-                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90 - r 16743 B1a"
+                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90 - r 16743 B1aY"
                 action = "sell"
                         
                 # 24 set 2022 ore 17:23
+                
+                
+                
+                
+                
+                
+                
                 
                 
             # 32 B2b - SELL condizione speciale ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90
