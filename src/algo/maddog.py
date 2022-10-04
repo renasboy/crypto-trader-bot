d4cebd2d2f1aa0d5b35d4f5d5f1a11e06dfb02a3
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-04 15:53:05 +0200

    4411 A e B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -5402,9 +5402,11 @@ class maddog:
                     
                     
                     deviation_ma100_sopra_ma300 > 0.50
+                    and ma5_last > ma150_last
+                    
                     and ma3_last > ma28_last
                     and ma200_last > ma200_20_min_ago
-                    and deviation_ma3_sopra_ma10 > 0.07
+                    and deviation_ma3_sopra_ma10 > 0.113
                     
                     and ma100_last > ma200_last
                     and ma200_last > ma300_last
