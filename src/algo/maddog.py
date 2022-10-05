fef853a3b21080a2abbcbefdc5befb76c8c61332
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-05 14:32:57 +0200

    16743 b2y

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -20247,16 +20247,33 @@ class maddog:
                 
                 
                 
-                
             elif (
                 seconds_since_last_trade > max_hold_time_in_seconds_sell_5_90
+                and deviation_ma100_sopra_ma300 > 0.20
                 and ma300_last < ma300_120_min_ago
                 and ma5_last < ma300_last
                 and ma2_last < ma2_2_min_ago
            
             ):
 
-                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90 - r 16743 B2"
+                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90 - r 16743 Bx"
+                action = "sell"
+                        
+                # 24 set 2022 ore 17:23
+                
+                
+                
+            elif (
+                seconds_since_last_trade > max_hold_time_in_seconds_sell_5_90
+                and deviation_ma100_sopra_ma300 < 0.20
+                and deviation_sell < -0.10
+                and ma300_last < ma300_120_min_ago
+                and ma5_last < ma300_last
+                and ma2_last < ma2_2_min_ago
+           
+            ):
+
+                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90 - r 16743 B2y"
                 action = "sell"
                         
                 # 24 set 2022 ore 17:23
