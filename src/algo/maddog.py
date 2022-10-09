aa5ce7f0869d33e3611bb10f848df1be0a3c6d3a
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-09 14:24:42 +0200

    1712 A e B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -2289,11 +2289,11 @@ class maddog:
                     
                 
                     
-                    
                 # BUY 1 con ma200 < 300< MA ma100> 100 60 min ago e doppio delta < and ma100 >60_min_ago STA RISALENDO !
             
                 elif (       
                     ma200_last < ma200_20_min_ago
+                    and ma250_last > ma300_last
                     and ma5_last > ma100_last
                     and ma2_last >= ma2_2_min_ago
                     
@@ -2307,7 +2307,42 @@ class maddog:
                     and ma300_last < ma300_60_min_ago
                 ):    
         
-                    buy = "BUY 1 5-100 con ma200< 300< MA ma100> 100 50 min ago e doppio delta < - GIORNO ! - riga 1712"
+                    buy = "BUY 1 5-100 con ma200< 300< MA ma100> 100 50 min ago e doppio delta < - GIORNO ! - riga 1712 a"
+                    action = "buy"
+                    percentage = 90
+                    
+                    # 11-150 perche' doppio delta sta risalendo !
+                    # MA HO DOVUTO AGGIUNGERE and deviation_ma5_sopra_ma28 > 0.20
+                    
+                    # 14 giu 2022 11-125 da 11-150 (giorno!)
+                    # 22 set 2022 aggiunto 3-10 0.07
+                    # 22 set 2022 5-28 > 0.12
+                    # 30 set 5-28 0.11 da 0.12
+                    #  7 ott 8-100 da 8-125
+                    #  8 ott ma100> da 50 min non da 60 min ! e' giorno !
+                    #  8 ott 5-28 a 0.10 da 0.11 ! e' giorno !
+                    
+                    
+                    
+                # BUY 1 con ma200 < and 300< MA ma100> 100 60 min ago e doppio delta < and ma100 >60_min_ago STA RISALENDO !
+            
+                elif (       
+                    ma200_last < ma200_20_min_ago
+                    and ma250_last < ma300_last
+                    and ma5_last > ma100_last
+                    and ma2_last >= ma2_2_min_ago
+                    
+                    and deviation_ma3_sopra_ma10 > 0.07
+                    and deviation_ma5_sopra_ma28 > 0.13
+                 
+                    and delta_1 < delta_2
+                    and ma100_last > ma100_50_min_ago
+                    
+                    and ma200_last < ma200_60_min_ago
+                    and ma300_last < ma300_60_min_ago
+                ):    
+        
+                    buy = "BUY 1 5-100 con ma200< and 300< MA ma100> 100 50 min ago e doppio delta < - GIORNO ! - riga 1712 b"
                     action = "buy"
                     percentage = 90
                     
