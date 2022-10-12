c2c74b0b38a193af11674c4d0293f685925e8ea3
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-12 19:45:06 +0200

    r1712

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -2310,7 +2310,7 @@ class maddog:
                     and ma2_last >= ma2_2_min_ago
                     
                     and deviation_ma3_sopra_ma10 > 0.07
-                    and deviation_ma5_sopra_ma28 > 0.09
+                    and deviation_ma5_sopra_ma28 > 0.08
                  
                     and delta_1 < delta_2
                     and ma100_last > ma100_50_min_ago
@@ -2334,7 +2334,7 @@ class maddog:
                     #  8 ott ma100> da 50 min non da 60 min ! e' giorno !
                     #  8 ott 5-28 a 0.10 da 0.11 ! e' giorno !
                     #  9 ott 5-28 a 0.09 da 0.10 ! e' giorno
-                    
+                    #  9 ott 5-28 a 0.08 da 0.09 ! e' giorno
                     
                 
             
