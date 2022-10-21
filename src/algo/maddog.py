4dcc02cbdc17be4d8e0990967db1fdbf6c8e60c5
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-21 13:59:21 +0200

    1043 B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1189,7 +1189,7 @@ class maddog:
                     and ma2_last > ma2_2_min_ago
               
                 ):
-                    buy = "BUY 1 5-300 - riga 1043 A"
+                    buy = "BUY 1 che considera distanza 5-300 - riga 1043 A"
                     action = "buy"
                     percentage = 50
                     
@@ -1204,6 +1204,7 @@ class maddog:
                 elif (
                     
                     ma8_last > ma69_last
+                    
                     and deviation_ma100_sopra_ma200 > -0.30
                     
                     and deviation_ma5_sotto_ma300 < 0.45
@@ -1214,19 +1215,26 @@ class maddog:
                     and ma200_last < ma200_60_min_ago
                     
                     
-                    and ma13_last > ma69_last
-                    and deviation_bellissima > 0.17
+                    and deviation_ma3_sopra_ma10 > 0.01
+                    and deviation_ma5_sopra_ma28 > 0.05
+                    and deviation_bellissima > 0.07
+                    
                     and deviation_ma13_sopra_ma25 > 0.07
                     and deviation_ma3_sopra_ma7 > 0.05
                     and ma3_last > ma3_3_min_ago
                     and ma2_last > ma2_2_min_ago
               
                 ):
-                    buy = "BUY 1 5-300 - riga 1043 B"
+                    buy = "BUY 1 che considera distanza 5-300 - riga 1043 B"
                     action = "buy"
                     percentage = 50
                     
                     # importante : ma100 sta sotto la ma200 non le e' troppo lontana
+                    # dev bellissima = 6 / 30
+                    # 21 ottobre 2022  aggiunta di 3-10 e 5-28
+                    # 21 ottobre 2022 3-10 = 0.01
+                    # 21 ottobre 2022 5-28 = 0.05
+                    # 21 ottobre 2022 6-30 = 0.07
                     
                     
                     
