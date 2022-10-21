7c344c5de6e4d84dec91f0e0c3f6e487d7955fd9
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-21 09:48:39 +0200

    conflitto risolto togliendo

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1553,7 +1553,7 @@ class maddog:
                 
                     
                     
-                # ------------------------------------------------------------ BUY 1 laterale 2 - r 1206 AA2x
+                # ------------------------------------------------------------ BUY 1 laterale 2 - r 1206 AA2
                 
                 elif (    
                     ma8_last > ma59_last
@@ -1565,7 +1565,7 @@ class maddog:
              
                 ):  
                 
-                    buy = "BUY 1 laterale 2 - r 1206 AA2x"
+                    buy = "BUY 1 laterale 2 - r 1206 AA2"
                     action = "buy"
                     percentage = 50
                     
@@ -1577,26 +1577,7 @@ class maddog:
                     
                 
                 
-                # ------------------------------------------------------------ BUY 1 laterale 2 - r 1206 AA2y
                 
-                elif (    
-                    ma8_last > ma59_last
-                    and ma300_last < ma450_last
-                    and deviation_ma100_sopra_ma300 < 0.30
-                    and deviation_ma100_sopra_ma300 > -0.20
-                    
-                    and deviation_ma3_sopra_ma10 > 0.05
-             
-                ):  
-                
-                    buy = "BUY 1 laterale 2 - r 1206 AA2y"
-                    action = "buy"
-                    percentage = 50
-                    
-                    # 19 set 2022 aggiunta 3-10 > 0.07
-                    #  3 ott 2022 3-10 0.06 da 0.07 
-                    #  3 ott 2022 100 sopra 300 a < 0.30 da < 0.20 perche' mentre 100 scende gia' e' partito il rialzo !
-                    # arriva tardi e ho tolto qualcosina.
                     
                     
                     
