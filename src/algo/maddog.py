4d632ef6718a2aac2b4ee36e7255412154d63d48
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-22 11:15:34 +0200

    vediamo... errore di sintassi r 16743 B2bxB

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -20691,12 +20691,11 @@ class maddog:
                 
                 
                 
-                
             # 32 B2b - SELL condizione speciale ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90
             
             elif (
                 seconds_since_last_trade > max_hold_time_in_seconds_sell_5_90
-                seconds_since_last_trade < max_hold_time_in_seconds_sell_5_180
+                and seconds_since_last_trade < max_hold_time_in_seconds_sell_5_180
                 and deviation_sell > 0.11
                 and deviation_ma5_sotto_ma300 < 0.25
                 and ma300_last > ma300_120_min_ago
@@ -20706,6 +20705,28 @@ class maddog:
             ):
 
                 sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 150 min fino a 300 min con 5-90 - r 16743 B2bxA"
+                action = "sell"
+                        
+                # 24 set 2022 ore 17:23
+                # 11 ott 2022 dev sell 0.10 da 0.15
+                # 21 ott 2022 dev sell 0.11 da 0.10
+                
+                
+                
+            # 32 B2b - SELL condizione speciale ROOT DOWN DEEP - ro cano VENDE dopo 150 min = 9000 sec con 5-90
+            
+            elif (
+                
+                seconds_since_last_trade < max_hold_time_in_seconds_sell_5_180
+                and deviation_sell > 0.11
+                and deviation_ma5_sotto_ma300 < 0.25
+                and ma300_last > ma300_120_min_ago
+                and ma5_last < ma180_last
+                and ma2_last < ma2_2_min_ago
+           
+            ):
+
+                sell = "SELL condizione speciale 32 B ROOT DOWN DEEP - ro cano VENDE dopo 300 min fino a 300 min con 5-180 - r 16743 B2bxB"
                 action = "sell"
                         
                 # 24 set 2022 ore 17:23
