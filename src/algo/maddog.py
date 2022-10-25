c157c82cd3bea1898f4921c4f22db3f7b9bc4be8
DNABM <36816435+DNABM@users.noreply.github.com>
2022-10-25 14:28:23 +0200

    r 1206 AA2 x1 e x2

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1562,13 +1562,13 @@ class maddog:
                     
                     
                     
-                
                 # ------------------------------------------------------------ BUY 1 laterale 2 - r 1206 AA2x
                 
                 elif (    
                     ma8_last > ma59_last
                     and ma300_last > ma450_last
                     and ma100_last > ma100_40_min_ago
+                    
                     and deviation_ma100_sopra_ma300 < 0.30
                     and deviation_ma100_sopra_ma300 > -0.20
                     
@@ -1576,7 +1576,32 @@ class maddog:
                     
                 ):  
                 
-                    buy = "BUY 1 laterale 2 - r 1206 AA2x"
+                    buy = "BUY 1 laterale 2 - r 1206 AA2x1"
+                    action = "buy"
+                    percentage = 50
+                    
+                    # 19 set 2022 aggiunta 3-10 > 0.07
+                    #  3 ott 2022 3-10 0.06 da 0.07 
+                    #  3 ott 2022 100 sopra 300 a < 0.30 da < 0.20 perche' mentre 100 scende gia' e' partito il rialzo !
+                    # arriva tardi e ho tolto qualcosina.
+                    
+                    
+                    
+                # ------------------------------------------------------------ BUY 1 laterale 2 - r 1206 AA2x
+                
+                elif (    
+                    ma8_last > ma59_last
+                    and ma300_last > ma450_last
+                    and ma100_last < ma100_40_min_ago
+                    
+                    and deviation_ma100_sopra_ma300 < 0.30
+                    and deviation_ma100_sopra_ma300 > -0.20
+                    
+                    and deviation_ma3_sopra_ma10 > 0.05
+                    
+                ):  
+                
+                    buy = "BUY 1 laterale 2 - r 1206 AA2x2"
                     action = "buy"
                     percentage = 50
                     
