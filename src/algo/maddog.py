2c232bbe26e09f08e18456cbcf692c2dda81e143
DNABM <36816435+DNABM@users.noreply.github.com>
2022-11-02 09:37:26 +0100

    risoluzione conflitto

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -14260,14 +14260,14 @@ class maddog:
                     
                     elif (
                         ma50_last < ma50_2_min_ago
-                        and ma200_last > ma200_301_min_ago
+                        
                         and ma2_last < ma2_2_min_ago
                         and deviation_sell < -0.31
                         and ma3_last < ma39_last
                         and ma100_last > ma100_60_min_ago
                         and ma78_last > ma200_last
                     ):
-                        sell = "SELL 1 dopo 110 min con ma50 < MA ma100 > and (deviation_sell < -0.27 and ma3_last < ma39_last) - r 10489 a1"
+                        sell = "SELL 1 dopo 110 min con ma50 < MA ma100 > and (deviation_sell < -0.27 and ma3_last < ma39_last) - r 10489 a"
                         action = "sell"
                         
                         # 3 luglio 2022 a 0.27 da 0.20
