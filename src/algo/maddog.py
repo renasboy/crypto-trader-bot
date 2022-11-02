9664eae41ad511f7c6789cbe4362aeb491a48e78
DNABM <36816435+DNABM@users.noreply.github.com>
2022-11-02 11:32:59 +0100

    r 6859 A1 e A2
    ricomincio a fare modifiche

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -9520,9 +9520,38 @@ class maddog:
                     #####################################################################
                     
                     
-                
                 elif (
                     ma28_last >= ma28_2_min_ago
+                    and ma200_last > ma200_301_min_ago
+                    and ma300_last > ma300_60_min_ago
+                    and deviation_ma250_sopra_ma300 > 0.04
+                  
+                    and deviation_buy3 > 0.005
+                    and delta_buy3_incrocio_ma3_ma8 > 0.04
+                    and deviation_ma4_sopra_ma30 > 0.05
+                    
+                    and ma3_last > ma8_last
+                    and ma3_last > ma78_last
+                    
+                    and ma2_last > ma2_2_min_ago
+                    and ma7_last > ma25_last
+                    
+                    and ma13_last > ma100_last
+                    and deviation_ma5_sopra_ma28 > 0.03
+                ):
+                    buy = "BUY 4B RIVOLUZIONARIO con ma78 > - r 6859 A1"
+                    action = "buy"
+                    percentage = 70
+                    
+                    # deviation_buy3 = ma4_last/ma30_last
+                    # deviation_ma4_sopra_ma100 > 0.25 arrivati al buy 4 DEVE AVERE UNA CERTA FORZA !
+                    # ma se 250 > 300 ! serve una forza minore
+                    
+                    
+                    
+                elif (
+                    ma28_last >= ma28_2_min_ago
+                    and ma200_last < ma200_301_min_ago
                     and ma300_last > ma300_60_min_ago
                     and deviation_ma250_sopra_ma300 > 0.04
                   
@@ -9539,7 +9568,7 @@ class maddog:
                     and ma13_last > ma100_last
                     and deviation_ma5_sopra_ma28 > 0.05
                 ):
-                    buy = "BUY 4B RIVOLUZIONARIO con ma78 > - r 6859 A"
+                    buy = "BUY 4B RIVOLUZIONARIO con ma78 > - r 6859 A2"
                     action = "buy"
                     percentage = 70
                     
