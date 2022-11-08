2e0ef8079efd01253bb5bd5fb2e738d684e04958
DNABM <36816435+DNABM@users.noreply.github.com>
2022-11-08 09:28:29 +0100

    formula MY COMPA anche nel BUY 3 - per sicurezza

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -8456,22 +8456,57 @@ class maddog:
                     and ma2_last > ma2_2_min_ago
                 ):      
               
-                    buy = "BUY 3A con ma69 > MA ma200 scende da 60 min ! and ma300_last > ma300_60_min_ago - r 6248"
+                    buy = "BUY 3A con ma69 > MA ma200 scende da 60 min ! and ma300_last > ma300_60_min_ago - r 6225"
                     action = "buy"
                     percentage = 80
                     
                     
                     
                     
+                
+                ####################################################################################
+                ####################################################################################
+                
+                
+                # BUY 3 migliore formula MY COMPA - che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-50 - (importata dal BUY 1 r 1043 B)
+                
+                elif (
+                    
+                    ma8_last > ma50_last
                     
                     
+                    and deviation_ma10_sopra_ma200 < -0.25
+                    and deviation_ma10_sopra_ma200 > -0.40
+                  
+                    and deviation_ma100_sopra_ma200 > -0.40
                     
+                    and deviation_ma5_sotto_ma300 < 0.45
+                    and deviation_ma5_sotto_ma300 > -0.75
                     
+                    and ma300_last < ma300_60_min_ago
+                    and ma200_last < ma200_60_min_ago
                     
+                    and deviation_ma3_sopra_ma10 > 0.01
+                    and deviation_ma5_sopra_ma28 > 0.04
                     
+                    and ma2_last >= ma2_2_min_ago
+              
+                ):
+                    buy = "BUY 3 migliore formula MY COMPA - che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-50 - riga 6243"
+                    action = "buy"
+                    percentage = 50
                     
+                    # importante : ma100 sta sotto la ma200 non le e' troppo lontana
                     
+                    # 21 ottobre 2022  aggiunta di 3-10 e 5-28
+                    # 21 ottobre 2022 3-10 = 0.01
+                    # 21 ottobre 2022 5-28 = 0.05
+                    # 28 ottobre 2022 5-28 = 0.04
+                    # ave comba
+                    # 31 ottobre aggiunta dev 10-200 - formula MY COMPA
                     
+                    ####################################################################################
+                    ####################################################################################
                     
                     
                     
