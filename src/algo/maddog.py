348759924128b3fcad720f9c8a36b78288ed7890
DNABM <36816435+DNABM@users.noreply.github.com>
2022-11-26 19:28:43 +0100

    r 15825 A e B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -20625,19 +20625,35 @@ class maddog:
                         
                         
                         #############################################################################################################################
+                        
                        
+                    elif (
+                        ma50_last >= ma50_2_min_ago
+                        and ma100_last > ma100_120_min_ago
+                        and ma5_last < ma59_last
+                        and deviation_sell > 0.25 and deviation_sell < 0.56
+                        and ma2_last < ma2_2_min_ago
+                       
+                    ):
+                        sell = "SELL 4-5-x (12-21 min) con ma50 > and 100 > 100 120 min ago and 5-59 and deviation_sell 0.25-0.56 - FINTA ALLA MARADONA - r 15825 A"
+                        action = "sell"
+                        
+                        # deviation_sell = ma3_last/last_trade_price
+                        
                         
                     elif (
                         ma50_last >= ma50_2_min_ago
+                        and ma100_last < ma100_120_min_ago
                         and ma5_last < ma25_last
                         and deviation_sell > 0.25 and deviation_sell < 0.56
                         and ma2_last < ma2_2_min_ago
                        
                     ):
-                        sell = "SELL 4-5-x (12-21 min) con ma50 > and 5-25 and deviation_sell 0.25-0.56 - FINTA ALLA MARADONA - r 15825"
+                        sell = "SELL 4-5-x (12-21 min) con ma50 > and 5-25 and 100 < 100 120 min ago and deviation_sell 0.25-0.56 - FINTA ALLA MARADONA - r 15825 B"
                         action = "sell"
                         
                         # deviation_sell = ma3_last/last_trade_price
+                        
                         
                         
                         
