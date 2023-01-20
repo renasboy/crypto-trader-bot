4a4b713fe7fa576118964a18da89d1dd25e4e573
DNABM <36816435+DNABM@users.noreply.github.com>
2023-01-20 12:37:05 +0100

    risoluzione conflitto

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1254,7 +1254,7 @@ class maddog:
                     
                     ma5_last > ma59_last
                     and deviation_ma5_sotto_ma300 < -0.22
-                    and deviation_ma5_sotto_ma300 > -0.40
+                    and deviation_ma5_sotto_ma300 > -0.75
                     
                     and deviation_ma10_sopra_ma200 < -0.22
                     
@@ -1268,7 +1268,7 @@ class maddog:
                     and ma2_last >= ma2_2_min_ago
               
                 ):
-                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-59 - riga 1043 B1"
+                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-59 - riga 1043 B"
                     action = "buy"
                     percentage = 50
                     
@@ -1288,29 +1288,6 @@ class maddog:
                     
                     
                     
-                elif (
-                    
-                    ma5_last > ma10_last
-                    
-                    and deviation_ma5_sotto_ma300 < -0.40
-                    and deviation_ma5_sopra_ma59 < -0.17
-                    
-                    and deviation_ma10_sopra_ma200 < -0.22
-                   
-                    and ma300_last < ma300_60_min_ago
-                    and ma200_last < ma200_60_min_ago
-                    
-                    and deviation_ma3_sopra_ma10 > 0.01
-                    and deviation_ma5_sopra_ma28 > 0.01
-                    
-                    and ma2_last >= ma2_2_min_ago
-              
-                ):
-                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-59 - riga 1043 B2"
-                    action = "buy"
-                    percentage = 50
-                    
-                   
                     
                     
                     
