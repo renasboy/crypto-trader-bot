d4d7f794143794d6f03e92e3849f82a15d67840e
DNABM <36816435+DNABM@users.noreply.github.com>
2023-01-29 14:29:10 +0100

    r 7144 A

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -9946,14 +9946,14 @@ class maddog:
                 if (
                     ma50_last >= ma50_2_min_ago
                     and ma100_last > ma300_last
-                    and ma28_last > ma28_30_min_ago
+                    
                     and ma78_last > ma150_last
                     
                     and deviation_ma5_sopra_ma28 > 0.08
                     
-                    and deviation_buy3 > 0.06
+                    and deviation_buy3 > 0.05
                     and ma3_last > ma13_last
-                    and deviation_ma7_sopra_ma40 > 0.09
+                    and deviation_ma7_sopra_ma40 > 0.08
                     and ma4_last > ma9_last
                     and ma4_last > ma50_last
                     
@@ -9963,14 +9963,14 @@ class maddog:
                     and ma2_last > ma2_2_min_ago
                 
                 ):
-                    buy = "BUY 5 con ma50 > AND 50>100 and ma28_last > ma28_30_min_ago and 5-28 > 0.08 CON ma100_last > ma300_last - riga 7144 a"
+                    buy = "BUY 5 con ma50 > AND 50>100 and ma28_last > ma28_30_min_ago and 5-28 > 0.08 CON ma100_last > ma300_last - r 7144 A"
                     action = "buy"
                     percentage = 70
                     
                     # 6 giu 2022 5-28 a 0.12 da 0.15
                     
                     # > estate anticipo buy 5-28 0.08 da 0.12
-                    
+                    # 29 gen 2023 anticipato ndecchiecella questo BUY 5 !
                     
                     
                     
