397b64c06285eba9b8858f65d035533c41851ebb
DNABM <36816435+DNABM@users.noreply.github.com>
2023-02-20 19:21:22 +0100

    r 14283 A1 e A2

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -18857,21 +18857,42 @@ class maddog:
                         # 25 sett 5-150 da 5-28
                     
                     
-                    
                     elif (    
                         ma50_last < ma50_2_min_ago
+                        and delta_450_300 < delta_450_300_60_min
                         and ma250_last > ma300_last
                         and deviation_ma100_sopra_ma300 < 0.20
                         and ma2_last < ma2_2_min_ago 
-                        and deviation_sell < -0.19
+                        and deviation_sell < -0.21
                         and ma3_last < ma39_last
                     ):
-                        sell = "SELL 3 dopo 90 min con ma50 < and ma100 NON DISTANTE dalla ma300 and (dev_sell < -0.19 and ma3_last < ma39_last) - r 14283 A"
+                        sell = "SELL 3 > 90 min GIORNO con ma50 < and ma100 NON DISTANTE dalla ma300 and (dev_sell < -0.21 and ma3_last < ma39_last) - r 14283 A1"
                         action = "sell"
                         
                         # SE ma100 RESTA VICINA ALLA ma300 - trend laterale - allora tieniti pronto anche con la deviation sell
                         # 28 ott 2022 dev sell a 0.17 da 0.15
                         #  1 nov 2022 se 250 > 300 dev sell 0.19 da 0.17
+                        # 20 feb 2023 doppio delta 450-300 GIORNO
+                        # 20 feb 2023 dev_sell a -0.21 da -0.19
+                        
+                    elif (    
+                        ma50_last < ma50_2_min_ago
+                        and delta_450_300 > delta_450_300_60_min
+                        and ma250_last > ma300_last
+                        and deviation_ma100_sopra_ma300 < 0.20
+                        and ma2_last < ma2_2_min_ago 
+                        and deviation_sell < -0.20
+                        and ma3_last < ma39_last
+                    ):
+                        sell = "SELL 3 > 90 min NOTTE con ma50 < and ma100 NON DISTANTE dalla ma300 and (dev_sell < -0.20 and ma3_last < ma39_last) - r 14283 A2"
+                        action = "sell"
+                        
+                        # SE ma100 RESTA VICINA ALLA ma300 - trend laterale - allora tieniti pronto anche con la deviation sell
+                        # 28 ott 2022 dev sell a 0.17 da 0.15
+                        #  1 nov 2022 se 250 > 300 dev sell 0.19 da 0.17
+                        # 20 feb 2023 doppio delta 450-300 NOTTE
+                        # 20 feb 2023 dev_sell a -0.20 da -0.19
+                        
                         
                     elif (    
                         ma50_last < ma50_2_min_ago
