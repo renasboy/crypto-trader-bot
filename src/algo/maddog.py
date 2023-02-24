9c369ac61a5a897e1a46d4cca0fa4cc2ad781940
DNABM <36816435+DNABM@users.noreply.github.com>
2023-02-24 22:14:45 +0100

    r 1043 B2 conflitto

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1393,11 +1393,7 @@ class maddog:
                     and deviation_ma5_sotto_ma450 < -1.20
                     
                     and deviation_ma10_sopra_ma200 < -0.25
-                    
-                  
-                    and deviation_ma100_sopra_ma200 > -0.40
-                    
-                    
+                 
                     and ma450_last < ma450_360_min_ago
                     and ma300_last < ma300_60_min_ago
                     and ma200_last < ma200_60_min_ago
