99581356f61a5b206bedd079dc86c6c0c6c709fd
DNABM <36816435+DNABM@users.noreply.github.com>
2023-02-24 22:25:09 +0100

    r1357r vediamo

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1338,7 +1338,8 @@ class maddog:
                     
                     ma5_last > ma28_last
                     and deviation_ma5_sotto_ma300 < -0.22
-                    and deviation_ma5_sotto_ma300 > -0.99
+                    and deviation_ma5_sotto_ma450 < -1.20
+                    
                     and delta_1_78_39 < delta_2_78_39_70_min
                     
                     and deviation_ma10_sopra_ma200 < -0.22
@@ -1385,28 +1386,7 @@ class maddog:
                     
                 # BUY 1 5-300 zona mediana
                 
-                elif (
-                    
-                    ma8_last > ma50_last
-                   
-                    
-                    and deviation_ma5_sotto_ma450 < -1.20
-                    
-                    
-                 
-                    and ma450_last < ma450_360_min_ago
-                    and ma300_last < ma300_60_min_ago
-                    and ma200_last < ma200_60_min_ago
-                    
-                    and deviation_ma3_sopra_ma10 > 0.01
-                    and deviation_ma5_sopra_ma28 > 0.04
-                    
-                    and ma2_last >= ma2_2_min_ago
-              
-                ):
-                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-50 - r 1043 B2"
-                    action = "buy"
-                    percentage = 50
+                
                     
                     
                     
