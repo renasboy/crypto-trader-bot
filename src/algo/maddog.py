d471e066bdc74893759ce2aaea4a27906b5260a9
DNABM <36816435+DNABM@users.noreply.github.com>
2023-03-16 12:02:18 +0100

    cano QUA FUNZIONA

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1692,7 +1692,7 @@ class maddog:
                     and deviation_ma100_sopra_ma300 > -0.30
                     
                     and deviation_ma3_sopra_ma10 > 0.05
-                    deviation_ma3_sopra_ma16 > 0.20
+                    and deviation_ma3_sopra_ma16 > 0.20
                     and deviation_ma4_sopra_ma28 > 0.04
                 ):  
                 
