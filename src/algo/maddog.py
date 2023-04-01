810b190a4e9c5b59937f644ffcfc7a4f2a92f9be
DNABM <36816435+DNABM@users.noreply.github.com>
2023-04-01 22:23:34 +0200

    r 5019 a

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -6693,8 +6693,8 @@ class maddog:
                 # BUY 2 DURANTE UN RIBASSO AUDI con 5-16 copiata da RCCR CHE E' ANDATA BENISSIMO ! 22 ago 2022
                 
                 elif (
-                    deviation_ma300_sopra_ma450 < 0.15
-                    and deviation_ma300_sopra_ma450 > -0.15
+                    deviation_ma300_sopra_ma450 > 0.15
+                    
                     
                     and deviation_buy_crollo_1 < -0.60
                     and deviation_buy_crollo_1 > -1.60
@@ -6710,12 +6710,32 @@ class maddog:
                     action = "buy"
                     percentage = 50
                     
+                    
+                elif (
+                    deviation_ma300_sopra_ma450 < 0.15
+                    and deviation_ma300_sopra_ma450 > -0.15
+                    
+                    and deviation_buy_crollo_1 < -0.60
+                    and deviation_buy_crollo_1 > -1.60
+                    
+                    and ma4_last > ma15_last
+                   
+                    and deviation_ma3_sopra_ma10 > 0.125
+                    and deviation_ma5_sopra_ma16 > 0.09
+                    
+                    and ma2_last > ma2_2_min_ago
+                    and ma78_last < ma200_last
+                    
+                ):
+                    buy = "BUY 2 DURANTE UN RIBASSO AUDI con 4-15 copiata da RCCR CHE E' ANDATA BENISSIMO ! 22 ago 2022 - r 5019 a"
+                    action = "buy"
+                    percentage = 50
+                    
                     # deviation_buy_crollo_1 = ma8_last / ma78_last
                     # questa cosa che 78 deve essere < 200 ha dell' incredibile. MA NON TOCCARE !
                     # 16 mar 2023 aggiunta 3-10 > 0.125 3-10 deve stare sopra 0.115
                     
-                    # se 300 e' attaccata alla 450 caso particolare crollo durante trend laterale.
-                    # 1 apr 2023 tolto 5-16 > 0.10 e aggiunto 4-15 al posto di 5-16
+                    
                     
                     
                     
