5bc525ead434b6d2662fc047c1582273d5fb93d3
DNABM <36816435+DNABM@users.noreply.github.com>
2023-04-06 15:33:20 +0200

    r 7203 B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -10487,6 +10487,7 @@ class maddog:
                     and ma78_last > ma150_last
                     
                     and deviation_ma3_sopra_ma10 > 0.05
+                    and deviation_ma3_sopra_ma16 > 0.165
                     and deviation_ma5_sopra_ma28 > 0.12
                     
                     and deviation_buy3 > 0.06
@@ -10512,7 +10513,7 @@ class maddog:
                     # 10 gen 2023 dev buy 3 a 0.06 da 0.07
                     # 10 gen 2023 dev 7-40 a 0.08 da 0.09
                     # aggiunta 3-10 a 0.05
-                    
+                    # # 6 apr 2023 aggiunta 3-16 > 0.165
                     
                     
                     
