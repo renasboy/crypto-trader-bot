1ce17cb48e14551f0cb50dd189d2dc8528a6f0cb
DNABM <36816435+DNABM@users.noreply.github.com>
2023-05-09 12:04:30 +0200

    QUA CANO FUNZIONA

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -18372,18 +18372,18 @@ class maddog:
                         and ma100_last < ma100_60_min_ago
                         
                         and deviation_ma39 < -0.17
-                        and deviation_sell < -0.10
+                        and deviation_sell < -0.11
                         and ma2_last < ma2_2_min_ago
                     ):
-                        sell = "SELL 2 da 60 a 90 min TREND < con PERDITA BASE con ma50 < con deviation_ma39 <-0.17 and dev_sell < -0.10 - r 11999 B"
+                        sell = "SELL 2 da 60 a 90 min TREND < con PERDITA BASE con ma50 < con deviation_ma39 <-0.17 and dev_sell < -0.11 - r 11999 B"
                         action = "sell"
                         
                         # and ma3_last < ma39_last
                         # and deviation_sell < 0.10
-                        # se non ha forza dopo 1 ora e' inutile continuare a sperare ! POI STIAMO GIA' AL SELL 2 -le ma hanno avuto piu' tempo di salire
+                        # se non ha forza dopo 1 ora e' inutile continuare a sperare ! POI STIAMO GIA' AL SELL 2 - le ma hanno avuto piu' tempo di salire
                         # qui non ho messo il crollo perche' dopo 40 min o gia' ha venduto o e' gia' risalita
                         # cuscino dell' angelo custode
-                        # > estate dev sell -0.10 da 0.10
+                        #  9 mag 2023 dev sell a -0.11 da -0.10
                         
                     
                     
