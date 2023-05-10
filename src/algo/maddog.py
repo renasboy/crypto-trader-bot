648820ce8b1df6f22fa8f7d229fcd4984321a6a2
DNABM <36816435+DNABM@users.noreply.github.com>
2023-05-10 10:34:42 +0200

    CONFLITTO r 1050 !

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1482,19 +1482,14 @@ class maddog:
                 elif (
                     
                     deviation_ma5_sopra_ma59 < -0.15
+                    and ma450_last > ma450_720_min_ago
+                    and ma300_last < ma300_60_min_ago
+                    and ma200_last < ma200_120_min_ago
                     
                     and deviation_ma5_sotto_ma300 < -0.23
                     and deviation_ma5_sotto_ma300 > -0.75
                     
-                    and deviation_ma10_sopra_ma200 < -0.25
-                    and deviation_ma10_sopra_ma200 > -0.40
-                  
-                    and deviation_ma100_sopra_ma200 > -0.40
                     
-                    
-                    and ma450_last > ma450_720_min_ago
-                    and ma300_last < ma300_60_min_ago
-                    and ma200_last < ma200_120_min_ago
                     
                     and deviation_ma3_sopra_ma10 > 0.01
                     and deviation_ma5_sopra_ma28 > 0.04
