454a8d43ebbd992e4a07f5a8ca972923143ba515
DNABM <36816435+DNABM@users.noreply.github.com>
2023-05-21 19:51:08 +0200

    r 5829 B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -8004,33 +8004,32 @@ class maddog:
                     
                     
                 elif (
-                    ma10_last >= ma10_2_min_ago
+                    ma5_last >= ma5_2_min_ago
+                    and ma5_last > ma50_last
+                    and ma200_last > ma200_30_min_ago
                     and ma300_last < ma450_last
                     
-                    and ma200_last > ma200_30_min_ago
                     and deviation > -0.40
                     
                     and deviation_ma3_sopra_ma10 > 0.10
-                    and ma33_last > ma78_last
                     and deviation_ma5_sopra_ma28 > 0.14
+                    and deviation_buy_ma2_sopra_ma13 > 0.10
                     
-                    and deviation_ma4_sopra_ma30 > 0.11
                     and ma3_last > ma13_last
                     and ma4_last > ma9_last
-                    and deviation_ma7_sopra_ma40 > 0.05
-                    and ma7_last > ma25_last
-                    
-                    and deviation_buy_ma2_sopra_ma13 > 0.10
                     and ma4_last > ma50_last
+                    and ma7_last > ma25_last
+                  
                     and ma2_last >= ma2_2_min_ago
-                    
+                    and macd >= macd_2_min_ago
                 ):  
                 
-                    buy = "BUY 2 ma300_last > ma450_last e con ma200 che sale da 30 min 33 > 78 ! and 3-10 > 0.10 - r 5829 B"
+                    buy = "BUY 2 ma300_last > ma450_last e con ma200 che sale da 30 min 5>50! and 3-10 > 0.10 - r 5829 B"
                     action = "buy"
                     percentage = 70
                     
-                    
+                    # 21 mag 2023 5-50 da 33-78 !
+                    # 21 mag 2023 aggiunta macd > macd 2 min ago
                     
                     
                     
