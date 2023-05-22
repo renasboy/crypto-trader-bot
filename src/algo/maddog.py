a111bb3c9b0d41c71b66579ee9fee725e2a72673
DNABM <36816435+DNABM@users.noreply.github.com>
2023-05-22 11:17:09 +0200

    r 10472 B2

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -16598,7 +16598,7 @@ class maddog:
                         
                     elif (    
                         ma50_last < ma50_2_min_ago
-                        and deviation_ma39 < -0.194
+                        and deviation_ma39 < -0.1935
                         and deviation_sell < -0.14
                         and deviation_ma5_sotto_ma300 < 0.30
                       
@@ -16607,7 +16607,7 @@ class maddog:
                         and ma100_last > ma100_60_min_ago
                         and ma2_last < ma2_2_min_ago
                     ):
-                        sell = "SELL 1 dopo 240 min con ma50 < MA ma100 > E ma300 < e con dev_ma39 < -0.194 and dev sell < -0.14 - r 10472 B2"
+                        sell = "SELL 1 dopo 240 min CON ma300 < e con ma50 < MA ma100 > e con dev_ma39 < -0.1935 and dev sell < -0.14 - r 10472 B2"
                         action = "sell"
                         
                         # ma39 NON DEVE VENDERE in fase laterale ma in alto
@@ -16618,7 +16618,9 @@ class maddog:
                         #  9 apr 2023 dev sell -0.12 da -0.10
                         # 12 apr 2023 dev sell -0.14 da -0.12
                         
-                        # 12 apr 2023 dev39 a 0.194 da 0.193
+                        # 22 mag 2023 dev39 a 0.194 da 0.193
+                        # 22 mag 2023 dev39 a 0.1935 da 0.194
+                        
                         
                         
                     # ma se ma100 > dopo 240 min
