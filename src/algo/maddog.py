7694e239408421aed575a744c449dd1b27df46ba
DNABM <36816435+DNABM@users.noreply.github.com>
2023-05-28 13:45:51 +0200

    CONFLITTO

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -5493,9 +5493,6 @@ class maddog:
                     and ma59_last > ma250_last
                     and macd > macd_2_min_ago
                     
-                    and ma200_last > ma200_15_min_ago
-                    
-                    
                     and delta_1 < delta_2
                     and ma100_last > ma100_60_min_ago
                     
