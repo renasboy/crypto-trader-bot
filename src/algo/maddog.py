01ce693a9091e5ce7ad855dc85dde96ec3d25571
DNABM <36816435+DNABM@users.noreply.github.com>
2023-05-30 22:02:51 +0200

    r 10274 A2y

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -16135,9 +16135,9 @@ class maddog:
                         # 27 apr 2023 dev sell -0.241 da -0.243 (dopo che con -0.243 ha fatto -0.86% !)
                         # 27 apr 2023 and deviation_trend_ma200 a > -0.15 da > -0.13
                         
-                        
-                    elif (     
+                     elif (     
                         ma50_last < ma50_2_min_ago
+                        and deviation_ma100_sopra_ma300 > 0.30
                         and ma300_last > ma300_120_min_ago
                         
                         and deviation_ma39 < -0.22
@@ -16149,10 +16149,30 @@ class maddog:
                         and ma2_last < ma2_2_min_ago
                         and macd < macd_2_min_ago
                     ):
-                        sell = "SELL 1 da 110 a 239 min E 300 > 120 min ago e con ma50 < and dev3-39 < -0.22 con > PERDITA TOLLERATA - r 10274 A2"
+                        sell = "SELL 1 da 110 a 239 min E 300 > 120 min ago e con ma50 < and dev3-39 < -0.22 con > PERDITA TOLLERATA - r 10274 A2x"
                         action = "sell"
                         
-                        # deviation_ma39 = ma4_last / ma39
+                        
+                        
+                    elif (     
+                        ma50_last < ma50_2_min_ago
+                        and deviation_ma100_sopra_ma300 < 0.30
+                        and ma300_last > ma300_120_min_ago
+                        
+                        and deviation_sell < -0.15
+                        
+                        and ma3_last < ma39_last
+                        
+                        and deviation_trend_ma200 > -0.12
+                        and ma200_last > ma300_last
+                        
+                        and ma2_last < ma2_2_min_ago
+                        and macd < macd_2_min_ago
+                    ):
+                        sell = "SELL 1 da 110 a 239 min E 300 > 120 min ago e con ma50 < and dev sell < -0.15 con > PERDITA TOLLERATA - r 10274 A2y"
+                        action = "sell"
+                        
+                        
                         # 24 apr 2023 deviation_ma39 < -0.22
                         # macd sui tempi lunghi
                         
