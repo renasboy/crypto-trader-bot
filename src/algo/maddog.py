b365af91fad282ee85d4d6a844bea5466e46d6da
DNABM <36816435+DNABM@users.noreply.github.com>
2023-06-04 16:03:06 +0200

    r 5981

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -8692,6 +8692,7 @@ class maddog:
                     # 22 set aggiunta di 3-10 > 0.11
                     
                     
+                    
                 # BUY 2 ultima condizione ! ma tutte negative MA BUY con 50-100 (integra r2505)
                 
                 elif (     
@@ -8714,6 +8715,37 @@ class maddog:
                     percentage = 70
                     
                     # aggiunta di 5-28 < 0.12
+                    
+                    
+                elif (
+                    
+                    ma4_last > ma18_last
+                    and macd >= macd_2_min_ago
+                    
+                    and deviation_ma5_sopra_ma200 < -0.30
+                    and deviation_ma5_sotto_ma300 < -0.30
+                    and deviation_ma5_sotto_ma450 < -0.30
+                    and deviation_ma50_sopra_o_sotto_ma100 < -0.01
+                    
+                    and deviation_ma100_sopra_ma300 < -0.10
+                    
+                    and ma300_last < ma300_60_min_ago
+                    and ma200_last < ma200_60_min_ago
+                    
+                    and deviation_ma3_sopra_ma10 > 0.01
+                    
+                    and deviation_ma5_sopra_ma28 < -0.05
+                    and ma2_last >= ma2_2_min_ago
+              
+                ):
+                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND dev 5-30 CON AGGIUNTA PENSIERO LATERALE - r 5981"
+                    action = "buy"
+                    percentage = 50
+                    
+                    
+                
+                
+                
                     
                     
                     
