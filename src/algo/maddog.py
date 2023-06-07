7e10836f9811c755a4cebb59d5cd2b872125831f
DNABM <36816435+DNABM@users.noreply.github.com>
2023-06-07 23:20:57 +0200

    r 1043 B2

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1417,6 +1417,8 @@ class maddog:
                 elif (
                     
                     ma4_last > ma18_last
+                     
+                    and ma78_last > ma78_60_min_ago 
                     and ma450_last < ma450_360_min_ago
                     and macd >= macd_2_min_ago
                     
@@ -1431,12 +1433,42 @@ class maddog:
                     and ma200_last < ma200_60_min_ago
                     
                     and deviation_ma3_sopra_ma10 > 0.081
-                    
+                    and deviation_ma3_sopra_ma16 > 0.09
                     and deviation_ma5_sopra_ma28 > -0.15
                     and ma2_last >= ma2_2_min_ago
               
                 ):
-                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND dev 5-30 - r 1043 B"
+                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND dev 5-30 - r 1043 B1"
+                    action = "buy"
+                    percentage = 50
+                    
+                    
+                elif (
+                    
+                    ma4_last > ma18_last
+                  
+                    and ma78_last < ma78_60_min_ago 
+                    and ma450_last < ma450_360_min_ago
+                    and macd >= macd_2_min_ago
+                    
+                    and deviation_ma5_sopra_ma200 < -0.30
+                    and deviation_ma5_sotto_ma300 < -0.30
+                    and deviation_ma5_sotto_ma450 < -0.30
+                    and deviation_ma50_sopra_o_sotto_ma100 < -0.01
+                    
+                    and deviation_ma100_sopra_ma300 < -0.10
+                    
+                    and ma300_last < ma300_60_min_ago
+                    and ma200_last < ma200_60_min_ago
+                    
+                    and deviation_ma3_sopra_ma10 > 0.081
+                    and deviation_ma3_sopra_ma16 > 0.12
+                  
+                    and deviation_ma5_sopra_ma28 > -0.15
+                    and ma2_last >= ma2_2_min_ago
+              
+                ):
+                    buy = "BUY 1 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND dev 5-30 - r 1043 B2"
                     action = "buy"
                     percentage = 50
                     
