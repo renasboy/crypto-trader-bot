db909c06b1cfb72ca4de5fabce4572db0c338af4
DNABM <36816435+DNABM@users.noreply.github.com>
2023-07-07 23:44:11 +0200

    r 12343 a e b

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -19491,15 +19491,27 @@ class maddog:
                         # ma 3-15 vendeva troppo presto. guardando il grafico da piu' lontano.
                     
                     
-                    
                     elif (
                         ma50_last > ma50_2_min_ago
+                        and ma300_last > ma300_120_min_ago
+                        and (ma3_prev > ma28_prev and ma3_last < ma28_last)
+                        and deviation_sell > 1.50 and deviation_sell < 2.70
+                        and ma2_last < ma2_2_min_ago
+                    ):
+
+                        sell = "SELL 2 90-110 min con ma50 > incrocio 3-28 con ma300 > 120_min_ago and deviation_sell 1.50 - 2.70 - r 12343a"
+                        action = "sell"
+
+                  
+                    elif (
+                        ma50_last > ma50_2_min_ago
+                        and ma300_last < ma300_120_min_ago
                         and (ma3_prev > ma18_prev and ma3_last < ma18_last)
                         and deviation_sell > 1.50 and deviation_sell < 2.70
                         and ma2_last < ma2_2_min_ago
                     ):
 
-                        sell = "SELL 2 90-110 min con ma50 > incrocio 3-18 and deviation_sell 1.50 - 2.70 - r 12343"
+                        sell = "SELL 2 90-110 min con ma50 > incrocio 3-18 con ma300 < 120_min_ago and deviation_sell 1.50 - 2.70 - r 12343b"
                         action = "sell"
                         
                         
