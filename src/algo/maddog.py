4777d6d6298d158a93bcc7cb1d7ffdf6899f151c
DNABM <36816435+DNABM@users.noreply.github.com>
2023-08-21 23:33:19 +0200

    r 16750 A2y

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -25655,10 +25655,11 @@ class maddog:
             ):    
                 sell = "SELL condizione POCHI MALEDETTI E SUBITO da 0 a 120 min con 3-13 E con dev_sell 0.70 - 1.05 and macd < - INTANTO TE LI PRENDI - r 16750 A1"
                 action = "sell"
-                
+
+          
                 
             elif (
-                seconds_since_last_trade > 0 and seconds_since_last_trade < 7200
+                seconds_since_last_trade > 0 and seconds_since_last_trade < 3600
                 and deviation_ma10__diviso_ma10_5_ore_ago < 1.50
                 and ma3_last < ma28_last
                 and deviation_ma3_di_adesso_diviso_ma3_di_90_min_ago < 1.30
@@ -25669,7 +25670,7 @@ class maddog:
                 and ma2_last < ma2_2_min_ago
                 and macd < macd_2_min_ago
             ):    
-                sell = "SELL condizione POCHI MALEDETTI E SUBITO da 0 a 120 min con 3-28 E con dev_sell 0.75 - 1.05 and macd < - INTANTO TE LI PRENDI - r 16750 A2"
+                sell = "SELL condizione POCHI MALEDETTI E SUBITO da 0 a 60 min con 3-28 E con dev_sell 0.75 - 1.05 and macd < - INTANTO TE LI PRENDI - r 16750 A2x"
                 action = "sell"
                 
                 # pochi 0.80 - 1.20
@@ -25687,6 +25688,25 @@ class maddog:
                 #  7 lug 2023 0.75 - 1.05 da 0.70 - 1.05
                 #  7 lug 2023 0.75 - 1.05 da 0.70 - 1.05
                 #  7 lug 2023 3-28 da 3-13
+          
+
+          
+            elif (
+                seconds_since_last_trade > 3601 and seconds_since_last_trade < 7200
+                and deviation_ma10__diviso_ma10_5_ore_ago < 1.50
+                and ma3_last < ma39_last
+                and deviation_ma3_di_adesso_diviso_ma3_di_90_min_ago < 1.30
+                
+                and deviation_sell > 0.75
+                and deviation_sell < 1.05
+                
+                and ma2_last < ma2_2_min_ago
+                and macd < macd_2_min_ago
+            ):    
+                sell = "SELL condizione POCHI MALEDETTI E SUBITO da 60 a 120 min con 3-39 E con dev_sell 0.75 - 1.05 and macd < - INTANTO TE LI PRENDI - r 16750 A2y"
+                action = "sell"
+                
+                
 
           
           
