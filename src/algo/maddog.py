1271bede4724fc12ce324e1cd4a2dc326c284168
DNABM <36816435+DNABM@users.noreply.github.com>
2023-10-21 15:42:09 +0200

    r 16047 B1x

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -27366,6 +27366,22 @@ class maddog:
 
 
 
+            elif (
+                ma78_last < ma78_120_min_ago
+                and ma300_last > ma300_301_min_ago
+                and ma50_last > ma50_2_min_ago
+                
+                and deviation_ma39 < 0.06
+                and deviation_sell < -0.107
+                
+                and deviation_ma5_sotto_ma200 > -0.64
+                
+                and ma2_last <= ma2_2_min_ago 
+                and macd < 5
+            ):
+                sell = "SELL cond spec SALVAGENTE 4 con ma 50 > ma 50 2 min ago con ma300 > 5 ore - dev 3-39 < 0.05 and dev_sell < -0.107 e ma5 sotto ma200 > -0.64 - r 16047 B1x"
+                action = "sell"
+
           
             elif (
                 ma78_last < ma78_120_min_ago
@@ -27380,7 +27396,7 @@ class maddog:
                 and ma2_last <= ma2_2_min_ago 
                 and macd < 5
             ):
-                sell = "SELL condizione speciale 6 con ma300 > 5 ore - SALVAGENTE 4 dev 3-39 < 0.05 and dev_sell < -0.107 - con ma50 < e con ma5 sotto ma200 > -0.64 - r 16047 B1"
+                sell = "SELL cond spec con ma 50 < ma 50 2 min ago  con ma300 > 5 ore - SALVAGENTE 4 dev 3-39 < 0.05 and dev_sell < -0.107  e ma5 sotto ma200 > -0.64 - r 16047 B1y"
                 action = "sell"   
                 
 
