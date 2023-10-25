b2e738e413f060f3e57ebb0222bb8b34ec6a6418
DNABM <36816435+DNABM@users.noreply.github.com>
2023-10-25 21:17:58 +0200

    r 9360

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -15889,8 +15889,10 @@ class maddog:
                         
                   
 
-                    # ----------------------------------------------------------------------------- CONDIZIONE SPECIALE NEL SELL 1 !
-                  
+                    # ------------------------------------------------------------------------------------------------------------------ CONDIZIONI SPECIALI NEL SELL 1 !
+
+                    
+                    
                     elif (
                         ma78_last < ma78_120_min_ago
                         and ma300_last > ma300_301_min_ago
@@ -15911,8 +15913,7 @@ class maddog:
                         sell = "SELL 1 (21-50 min) cond spec EX r 16047 B2 con ma300> 5 ore - SALVAGENTE 4 deviation 3-39 < 0.05 and dev_sell < -0.10 - con ma50 <  - r 9355a"
                         action = "sell"
 
-
-                  
+                    
                     elif (
                         ma78_last < ma78_120_min_ago
                         and ma50_last <= ma50_2_min_ago
@@ -15932,6 +15933,33 @@ class maddog:
                         action = "sell"
                         # 28 set 2023 dev sell -0.07 da -0.06
                         # 28 set 2023 and macd a -3 da -2
+
+                  
+                    
+                    elif (
+                        ma78_last < ma78_120_min_ago
+                        and ma300_last > ma300_301_min_ago
+                        and ma50_last <= ma50_2_min_ago
+                
+                        and deviation_ma39 < 0.06
+                        and deviation_sell < -0.05
+                
+                        and deviation_ma5_sotto_ma200 > -0.64
+                
+                        and ma2_last <= ma2_2_min_ago 
+                        and macd < 5
+                    ):
+                        sell = "SELL (21-50 min) EX cond spec r 16047 B1y con ma 50 < ma 50 2 min ago  con ma300 > 5 ore - dev 3-39 < 0.05 and dev_sell < -0.05 - r 9360"
+                        action = "sell"
+                        
+                        # 25 ott 2023 and dev_sell < -0.05 da < -0.107
+
+                    
+                    
+
+
+                  
+                    
 
                   
                     
