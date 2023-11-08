815bd43d00c05cf6c665a6298fe6b6def44e1016
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-08 09:23:06 +0100

    r 16875 A

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -30289,18 +30289,18 @@ class maddog:
                 
                 and macd < macd_2_min_ago
                 and macd < -9
-                and macd_differenza < -24
+                and macd_differenza < -11
                 
                 and ma2_last < ma2_2_min_ago
                  
             ):    
-                sell = "SELL condizione speciale 42 - POCHI MALEDETTI E SUBITO CON ma300 che sale da 5 ore ! e con deviation > 1.01 e macd < -10 e MACD DIFFERENZA < -25 - r 16875 A"
+                sell = "SELL condizione speciale 42 - POCHI MALEDETTI E SUBITO CON ma300 che sale da 5 ore ! e con deviation > 1.01 e macd < -10 e MACD DIFFERENZA < -11 - r 16875 A"
                 action = "sell"
                 
                 #  1 set 2023 se per es macd passa da 18 a -11 vendi     
                 # 12 set 2023 macd < -9 da -10
                 # 12 set 2023 macd DIFFERENZA -24 da -25
-
+                #  8 nov 2023 macd DIFFERENZA -11 da -24
           
             elif (
                 deviation_sell > 0.91
