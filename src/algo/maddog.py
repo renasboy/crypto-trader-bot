3984395e0dc21b91c342bf0fc157093c1e78da96
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-16 17:30:32 +0100

    r 9931 B2 x e y

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -17678,12 +17678,15 @@ class maddog:
                         action = "sell"
                         
                         #  7 nov 2023 se 300< 5 ore MA 100> 1 ora vendi dopo FORSE IL PEGGIO E' PASSATO !
+
+
+
                   
                     elif (
-                        ma78_last < ma78_120_min_ago
-                        and ma100_last < ma100_60_min_ago
-                        and ma300_last < ma300_301_min_ago
-                
+                        
+                        ma300_last < ma300_301_min_ago
+                        and ma50_last > ma50_2_min_ago
+                        
                         and deviation_ma39 < 0.05
                         and deviation_sell < -0.01
                 
@@ -17691,17 +17694,53 @@ class maddog:
                         and ma50_last <= ma50_2_min_ago
                         and deviation_ma100_sopra_ma300 < 0.40
                 
-                        and deviation_ma5_sotto_ma200 > -0.90
+                        and deviation_ma5_sotto_ma200 > -0.70
                 
                         and ma2_last <= ma2_2_min_ago 
                         and macd < macd_2_min_ago
                         and macd < -2
                     ):
-                        sell = "SELL 1 (50-90 min) ex cond. spec. r 16047 B2 con ma300> 5 ore - dev 3-39 < 0.05 e dev_sell < -0.01 - con ma50 < e ma5 sotto ma200 > -0.90 - r 9931 B2"
+                        sell = "SELL 1 (50-90 min) ex cond. spec. r 16047 B2 con ma300> 5 ore - dev 3-39 < 0.05 e dev_sell < -0.01 - con ma50 < e ma5 sotto ma200 > -0.70 - r 9931 B2x"
                         action = "sell"
                         
                         #  7 nov 2023 se 300< 5 ore E 100< 1 ora vendi prima NON TE NE FOTTERE
-                    
+                        # 16 nov 2023 ha fatto -0.78 ALLORA HO PRESO PROVVEDIMENTI DRASTICI
+                        # HO MESSO con ma50>
+                        # HO TOLTO CON 100< 100 60 min ago
+                        # HO TOLTO CON ma78< 120 min ago
+                        # ho portato ma5 sotto 200 a > -0.70 da -0.90
+                        # E VAFANGULO
+
+                  
+
+
+                  
+                    elif (
+                        
+                        ma300_last < ma300_301_min_ago
+                        and ma50_last <= ma50_2_min_ago
+                        
+                        and deviation_ma39 < 0.05
+                        and deviation_sell < -0.01
+                        
+                        and deviation_ma100_sopra_ma300 < 0.40
+                
+                        and deviation_ma5_sotto_ma200 > -0.70
+                
+                        and ma2_last <= ma2_2_min_ago 
+                        and macd < macd_2_min_ago
+                        and macd < -2
+                    ):
+                        sell = "SELL 1 (50-90 min) ex cond. spec. r 16047 B2 con ma300< 5 ore - dev 3-39 < 0.05 e dev_sell < -0.01 - con ma50 < e ma5 sotto ma200 > -0.90 - r 9931 B2y"
+                        action = "sell"
+                        
+                        #  7 nov 2023 se 300< 5 ore E 100< 1 ora vendi prima NON TE NE FOTTERE
+                        # 16 nov 2023 ha fatto -0.78 ALLORA HO PRESO PROVVEDIMENTI DRASTICI
+                        # HO MESSO con ma50>
+                        # HO TOLTO CON 100< 100 60 min ago
+                        # HO TOLTO CON ma78< 120 min ago
+                        # ho portato ma5 sotto 200 a > -0.70 da -0.90
+                        # E VAFANGULO
 
                     elif (
                         ma78_last < ma78_120_min_ago
