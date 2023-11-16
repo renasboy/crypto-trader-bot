bd9bde9b516d2d5377565e757126697661aa8db9
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-16 20:17:43 +0100

    28809 r da ridurre

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -20787,6 +20787,30 @@ class maddog:
 
 
                     ############################################################################################################# CONDIZIONI SPECIALI IMPORTATE RIDOTTE
+
+                  
+                    elif (
+                        ma78_last < ma78_120_min_ago
+                        and ma300_last < ma300_301_min_ago
+                        and delta_150_100 <= delta_150_100_60_min
+                
+                        and deviation_ma39 < 0.05
+                        and deviation_sell < -0.109
+                
+                
+                        and ma50_last <= ma50_2_min_ago
+                        and deviation_ma100_sopra_ma300 < 0.40
+                
+                        and deviation_ma5_sotto_ma200 > -0.90
+                
+                        and ma2_last <= ma2_2_min_ago 
+                        and macd < macd_2_min_ago
+                    ):
+                        sell = "SELL 2 (5-12 min) EX cond spec r 16047 B2 GIORNO con ma300> 5 ore - dev 3-39 < 0.05 and dev_sell < -0.109 - con ma50 < e con ma5 sotto ma200 > -0.90 - 11079"
+                        action = "sell"
+                        
+                        # DA RIDURRE
+
                     
                     elif (
                         ma78_last > ma78_120_min_ago
