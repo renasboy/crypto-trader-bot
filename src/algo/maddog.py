dac8c92bbe2e91fedba07857bff4f9ef051a59c2
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-20 10:48:00 +0100

    r 7589 a E b

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -14365,11 +14365,45 @@ class maddog:
                 
 
                 # BUY 5 compare forever EX BUY 1 r 1537 A2y1- con 5-20 E CON 300 ancora vicina alla 450 ( anche un poco sotto !
-              
+                elif (     
+                    
+                    ma3_last > ma13_last
+                    
+                    and ma100_last < ma100_120_min_ago
+                    and ma100_last > ma100_120_min_ago
+                    
+                    
+                    and deviation_ma54_sopra_o_sotto_ma72 < 0.015
+                    and deviation_ma54_sopra_o_sotto_ma72 > -0.015
+                    
+                    and deviation_ma39_sotto_ma200 < 0.10
+                    and deviation_ma300_sopra_ma450 > -0.35
+                    
+                    and deviation_ma3_sopra_ma10 > -0.015
+                    and deviation_ma3_sopra_ma16 > 0.01
+                    and deviation_ma3_sopra_ma18 > -0.03
+                    
+                    and ma2_last >= ma20_last
+                  
+                    and ma2_last >= ma2_2_min_ago
+                    and macd >= macd_2_min_ago
+                    and macd > -40
+                    and ma50_differenza_ma59 > -5
+                    
+                ):    
+                    buy = "BUY 5 compare forever EX BUY 1 r 1537 A2y1 - 100<120 min ago MA 100>60 min ago con 5-20 E 300 vicina alla 450 + MACD > E ma50_diff_ma59 > -5 - r 7589a"
+                    action = "buy"
+                    percentage = 90
+                    
+                    # 20 nov 2023 ma100 < 100 120 min ago MA ma 100> 60 min allora compra una ndecchiecella prima
+
+                
                 elif (     
                     
                     ma3_last > ma13_last
                     and ma100_last < ma100_120_min_ago
+                    and ma100_last < ma100_120_min_ago
+                    
                     
                     and deviation_ma54_sopra_o_sotto_ma72 < 0.015
                     and deviation_ma54_sopra_o_sotto_ma72 > -0.015
@@ -14389,7 +14423,7 @@ class maddog:
                     and ma50_differenza_ma59 > -5
                     
                 ):    
-                    buy = "BUY 5 compare forever EX BUY 1 r 1537 A2y1- con 5-20 E CON 300 ancora vicina alla 450 ( anche un poco sotto ! ) + MACD > and ma50_diff_ma59 > -5 - r 7589"
+                    buy = "BUY 5 compare forever EX BUY 1 r 1537 A2y1 - 100<120 min ago E 100<60 min ago con 5-20 E CON 300 vicina alla 450 + MACD > E ma50_diff_ma59 > -5 - r 7589b"
                     action = "buy"
                     percentage = 90
 
