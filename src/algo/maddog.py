a2f34601e7982ab95da0d037cc19f2d86b49b820
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-20 16:49:44 +0100

    r 16750 A2X

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -31020,14 +31020,14 @@ class maddog:
                 and ma3_last < ma28_last
                 and deviation_ma3_di_adesso_diviso_ma3_di_90_min_ago < 1.30
                 
-                and deviation_sell > 0.75
+                and deviation_sell > 0.70
                 and deviation_sell < 1.05
                 
                 and ma2_last < ma2_2_min_ago
                 and macd < macd_2_min_ago
                 and ma2_differenza_2_min_ago < -3
             ):    
-                sell = "SELL condizione POCHI MALEDETTI E SUBITO da 0 a 60 min con 3-28 E con dev_sell 0.75 - 1.05 and macd < - INTANTO TE LI PRENDI - r 16750 A2X"
+                sell = "SELL condizione POCHI MALEDETTI E SUBITO da 0 a 60 min con 3-28 E con dev_sell 0.70 - 1.05 and macd < - INTANTO TE LI PRENDI - r 16750 A2X"
                 action = "sell"
                 
                 # pochi 0.80 - 1.20
@@ -31046,7 +31046,9 @@ class maddog:
                 #  7 lug 2023 0.75 - 1.05 da 0.70 - 1.05
                 #  7 lug 2023 3-28 da 3-13
                 # 10 ago 2023 aggiunta and ma2_differenza_2_min_ago < -3
+                # 20 nov 2023 and deviation_sell > 0.70 da > 0.75
 
+          
             # 3
             
             elif (
