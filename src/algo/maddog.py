f1cafd44df89d1001887b374173dd01178c43dda
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-25 12:44:30 +0100

    r 6571

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -11394,7 +11394,39 @@ class maddog:
                     # 16 nov 2023 NON HA PRESO LA DIFFERENZA ma2 allora ho portato sopra. vediamo
                     # 16 nov 2023 aggiunta and deviation_ma3_sopra_ma10 > 0.018 (anche perche' non ha preso la differenza ma2)
               
-              
+                
+
+                # BUY 3 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-50 - formula MY COMPA (importata dal BUY 1 r 1043 B)
+                
+                elif (
+                    
+                    ma8_last > ma50_last
+                    and delta_1 < delta_2
+                    
+                    and deviation_ma10_sopra_ma200 < -0.25
+                    and deviation_ma10_sopra_ma200 > -0.40
+                  
+                    and deviation_ma100_sopra_ma200 > -0.40
+                    
+                    and deviation_ma5_sotto_ma300 < 0.45
+                    and deviation_ma5_sotto_ma300 > -0.75
+                    
+                    and ma300_last < ma300_60_min_ago
+                    and ma200_last < ma200_60_min_ago
+                    
+                    and deviation_ma3_sopra_ma10 > 0.01
+                    and deviation_ma5_sopra_ma28 > 0.04
+                    
+                    and ma2_last >= ma2_2_min_ago
+                    and macd >= macd_2_min_ago
+                ):
+                    buy = "BUY 3 migliore IMPORTATO dal BUY 2 r 4743 A de RCCR che considera distanza 5-300 AND dev 10-200 < -0.24 AND 8-50 - formula MY COMPA - r 6571"
+                    action = "buy"
+                    percentage = 80
+                    
+                    # importante : ma100 sta sotto la ma200 MA non le e' troppo lontana
+
+                
                 # BUY 3 29 ago 2022 > 180 min di ribasso
                 
                 elif (
