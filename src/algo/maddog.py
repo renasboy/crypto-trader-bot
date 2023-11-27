3a8e44c94659901b8a0e4b489f706635a4041013
DNABM <36816435+DNABM@users.noreply.github.com>
2023-11-27 12:16:52 +0100

    r 4710

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -7068,9 +7068,8 @@ class maddog:
                     
                 # BUY 2 CHE MANCAVA DOPO BUY-SELL CROLLO ! 150-100 GIORNO ! E 50 > 100
                 
-                elif (
-                    
-                    ma5_last > ma59_last
+                elif (    
+                    ma5_last > ma50_last
                     and ma100_last >= ma100_2_min_ago
                     
                     and ma100_last < ma100_120_min_ago
@@ -7082,7 +7081,8 @@ class maddog:
                     and ma200_last < ma300_last
                     
                     and delta_150_100 <= delta_150_100_60_min
-                    and deviation_ma10_sopra_ma200 > -0.20
+                    
+                    and deviation_ma10_sopra_ma200 > -0.25
                     
                     and deviation_ma3_sopra_ma10 > 0.01
                     and deviation_ma5_sopra_ma28 > 0.01
@@ -7090,7 +7090,7 @@ class maddog:
                     and ma2_last >= ma2_2_min_ago
                     and macd >= macd_2_min_ago
                 ):
-                    buy = "BUY 2 78<200 CHE MANCAVA DOPO BUY-SELL CROLLO ! 50 > 100 AND 150-100 GIORNO ! and 3-10 > 0.15 and 5-28 > 0.18 and macd > macd 2 min ago - r 4710"
+                    buy = "BUY 2 con 150-100 GIORNO ! e con 78<200 CHE MANCAVA DOPO BUY-SELL CROLLO ! and 3-10 > 0.01 and 5-28 > 0.01 and macd > macd 2 min ago - r 4710"
                     action = "buy"
                     percentage = 80
                     
