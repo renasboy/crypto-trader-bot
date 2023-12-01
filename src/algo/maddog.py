0a121218485e7f69fb8bb5ccf26d557612104f2f
DNABM <36816435+DNABM@users.noreply.github.com>
2023-12-01 21:35:40 +0100

    r 7589 B

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -14532,13 +14532,14 @@ class maddog:
 
                 
 
-                # BUY 5 compare forever EX BUY 1 r 1537 A2y1- con 5-20 E CON 300 ancora vicina alla 450 ( anche un poco sotto !
+                # BUY 5 compare forever EX BUY 1 r 1537 A2y1 - con 5-20 E CON 300 ancora vicina alla 450 ( anche un poco sotto !)
+                
                 elif (     
                     
                     ma3_last > ma13_last
                     
                     and ma100_last < ma100_120_min_ago
-                    and ma100_last > ma100_120_min_ago
+                    and ma100_last > ma100_60_min_ago
                     
                     
                     and deviation_ma54_sopra_o_sotto_ma72 < 0.015
@@ -14572,18 +14573,18 @@ class maddog:
                     and ma2_differenza_2_min_ago > 3
                     
                     and ma100_last < ma100_120_min_ago
-                    and ma100_last < ma100_120_min_ago
+                    and ma100_last < ma100_60_min_ago
                     
                     
                     and deviation_ma54_sopra_o_sotto_ma72 < 0.015
                     and deviation_ma54_sopra_o_sotto_ma72 > -0.015
                     
                     and deviation_ma39_sotto_ma200 < 0.10
-                    and deviation_ma300_sopra_ma450 > -0.35
+                    and deviation_ma300_sopra_ma450 > -0.37
                     
                     and deviation_ma3_sopra_ma10 > -0.01
                     and deviation_ma3_sopra_ma16 > 0.01
-                    and deviation_ma3_sopra_ma18 > -0.02
+                    and deviation_ma3_sopra_ma18 > -0.03
                     
                     and ma2_last >= ma20_last
                   
