485bafcc78df309559a3a2c64147e64a239ebfb2
DNABM <36816435+DNABM@users.noreply.github.com>
2023-12-12 13:48:33 +0100

    r 7270

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -13966,7 +13966,44 @@ class maddog:
                     
                     
                     
-                 
+                elif (     
+                    
+                    ma3_last > ma20_last
+                    and ma3_last < ma39_last
+                    and ma300_last > ma300_180_min_ago
+                    
+                    and ma100_last < ma100_60_min_ago
+                    and ma100_last < ma100_120_min_ago
+                  
+                    and deviation_ma54_sopra_o_sotto_ma72 < -0.015
+                    
+                    and deviation_ma39_sotto_ma200 < -0.01
+                    and deviation_ma300_sopra_ma450 > -0.50
+                    
+                    and deviation_ma3_sopra_ma10 > 0.017
+                    and deviation_ma3_sopra_ma16 > 0.025
+                    and deviation_ma3_sopra_ma18 > -0.03
+                    
+                    and ma2_last >= ma20_last
+                    
+                    and ma2_last >= ma2_2_min_ago
+                    and ma2_differenza_2_min_ago > 3
+                    and ma2_differenza_ma5 > 26
+                    and macd > -10
+                    and macd >= macd_2_min_ago
+                    
+                ):    
+                    buy = "BUY 5 da r 1537 A3X 4A compare forever + 300>180 min and 3>39 con 3-20 E 300 vicina 450 ( anche poco sotto ! ) e 3-16 > 0.01 + MACD > E MACD > -10 - r 7270"
+                    action = "buy"
+                    percentage = 90
+                    
+                    # 15 nov 2023 se compra quando 3<39 allora riduco dev 3-16 
+                    # 16 nov 2023 AGGIUNTO SOLO SU QUESTA CONDIZIONE E DOPO ATTENTA ANALISI STATISTICA and macd > 8
+                    #  5 dic 2023 aggiunta and ma2_differenza_ma5 > 26 vedi 4 dic 2023 ore 16:45
+                    #  5 dic 2023 macd >-8 da >8
+                    # 12 dic 2023 macd a -10 da -8
+
+              
                 # BUY 5 di emergenza CROLLO FERRARI - modo 1
                 
                 elif (
