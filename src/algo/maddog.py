7797e25674385d83b2d3061c6d4f322fd0e7e399
DNABM <36816435+DNABM@users.noreply.github.com>
2023-12-24 17:57:43 +0100

    r 10263 A2 x1

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -19984,11 +19984,11 @@ class maddog:
                     elif (
                         ma5_last < ma150_last
                         
-                        and deviation_ma39 < -0.15
+                        and deviation_ma39 < -0.01
                         and deviation_sell > 0.20
                         and ma5_last < ma5_2_min_ago
                       
-                        and ma50_last < ma50_2_min_ago
+                        and ma50_last <= ma50_2_min_ago
                         and ma150_last > ma150_20_min_ago
                         and ma300_last > ma300_120_min_ago
                         and ma100_last > ma100_60_min_ago
@@ -19996,7 +19996,7 @@ class maddog:
                         and ma2_last < ma2_2_min_ago
                         and ma2_differenza_2_min_ago < -0.05
                     ):
-                        sell = "SELL 1 da 110 a 239 min con ma50 < MA ma100 > E ma300 > e con deviation_ma39 < -0.15 and ma2_differenza_2_min_ago < -0.05 - r 10263 A2x1"
+                        sell = "SELL 1 da 110 a 239 min con ma50 <= MA ma100 > E ma300 > e con deviation_ma39 < -0.15 and ma2_differenza_2_min_ago < -0.05 - r 10263 A2 x1"
                         action = "sell"
                         
                         # ma39 NON DEVE VENDERE in fase laterale ma in alto
@@ -20004,8 +20004,10 @@ class maddog:
                         # 16 mar 2023 ma39 <-0.1965 da <-0.197
                         #  9 nov 2023 ma39 <-0.15 da <-0.1965
                         #  9 nov 2023 aggiunta and ma2_differenza_2_min_ago < -0.05
-                  
-                        
+                        # 24 dic 2023 ma39 a -0.05 da -0.15
+                        # 24 dic 2023 and ma50_last <= ma50_2_min_ago da ma50_last < ma50_2_min_ago
+
+                    
                     elif (    
                         ma50_last < ma50_2_min_ago
                         and ma300_last > ma300_120_min_ago
