fae9886bac84dba5e259f1f9be307783f4af430c
DNABM <36816435+DNABM@users.noreply.github.com>
2023-12-26 14:24:42 +0100

    r 16045 A x e y

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -31206,9 +31206,10 @@ class maddog:
                 
             # 5 - SELL condizione speciale ro cano VENDE CON UN SALVAGENTE con deviation_ma5_sotto_ma200 > -0.50
             
-            
             elif (
                 ma78_last > ma78_120_min_ago
+                and ma10_last >= ma20_last
+                and ma50_last <= ma50_2_min_ago
                 
                 and ma2_last < ma2_2_min_ago 
                 and ma2_differenza_2_min_ago < -7
@@ -31219,17 +31220,41 @@ class maddog:
                 and deviation_ma39 < 0.07
                 and deviation_sell < -0.10
                 
-                and ma50_last <= ma50_2_min_ago
-                and deviation_ma100_sopra_ma300 < 0.40
                 
+                and deviation_ma100_sopra_ma300 < 0.40
                 and deviation_ma5_sotto_ma200 > -0.50
-                  
+                
             ):
-                sell = "SELL condizione speciale 6 GIORNO con ma300 < 5 ore - dev 3-39 < 0.07 and dev_sell < -0.10 e con ma50 < e con ma78 > and diff ma2 < -7 - r 16045 A"
+                sell = "SELL cond spec 6 GIORNO con ma300 < 5 ore MA CON 10>=20 and dev 3-39 < 0.07 and dev_sell < -0.09 e con ma50 < e con ma78 > and diff ma2 < -7 - r 16045 Ax"
+                action = "sell"
+                
+                # 26 dic 2023 con 10>=20 vendi dopo a -0.09 da -0.10
+                
+            
+            elif (
+                ma78_last > ma78_120_min_ago
+                and ma10_last < ma20_last
+                and ma50_last <= ma50_2_min_ago
+                
+                and ma2_last < ma2_2_min_ago 
+                and ma2_differenza_2_min_ago < -7
+                
+                and ma300_last < ma300_301_min_ago
+                and delta_150_100 <= delta_150_100_60_min
+                
+                and deviation_ma39 < 0.07
+                and deviation_sell < -0.03
+                
+                and deviation_ma100_sopra_ma300 < 0.40
+                and deviation_ma5_sotto_ma200 > -0.50
+                
+            ):
+                sell = "SELL cond spec 6 GIORNO con ma300 < 5 ore e con 10<20 and dev 3-39 < 0.07 and dev_sell < -0.03 e con ma50 < e con ma78 > and diff ma2 < -7 - r 16045 Ay"
                 action = "sell"
                 
                 # 20 nov 2023 aggiunta and diff ma2 < -5 
                 # 10 dic 2023 and deviation_sell a -0.10 da -0.0107
+                # 26 dic 2023 con 10<20 vendi prima a -0.03 da -0.10
 
             elif (
                 ma78_last < ma78_120_min_ago
