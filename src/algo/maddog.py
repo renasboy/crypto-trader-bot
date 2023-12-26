2e3a0fb0c4eef1bc77ddecc299a2beb46b1d4b67
DNABM <36816435+DNABM@users.noreply.github.com>
2023-12-26 12:53:43 +0100

    r 4713

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -7314,7 +7314,6 @@ class maddog:
                     ma3_last > ma18_last
                     and ma450_last < ma450_360_min_ago
                     
-                    
                     and deviation_ma5_sopra_ma200 < -0.30
                     and deviation_ma5_sotto_ma300 < -0.30
                     and deviation_ma5_sotto_ma450 < -0.30
@@ -7325,12 +7324,14 @@ class maddog:
                     and ma300_last < ma300_60_min_ago
                     and ma200_last < ma200_60_min_ago
                     
-                    and deviation_ma3_sopra_ma10 > 0.02
-                    and deviation_ma3_sopra_ma16 > 0.08
+                    and deviation_ma3_sopra_ma10 > 0.01
+                    and deviation_ma3_sopra_ma16 > 0.07
                     and deviation_ma5_sopra_ma28 > -0.15
                     
                     and ma2_last >= ma2_2_min_ago
-                    and macd > -3
+                    and deviation_ma2_sopra_o_sotto_ma2_2_min_ago > 0.018
+                    and macd > -30
+                    and macd_differenza_2_min_ago > 1.30
                 ):
                     buy = "BUY 2 migliore che considera distanza 5-300 AND dev 10-200 < -0.24 AND dev 4-18 and deviation_ma3_sopra_ma16 > 0.12 - r 4713"
                     action = "buy"
