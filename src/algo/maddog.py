41e84463cdbbb8bbd1591c9de901e5862fc62311
DNABM <36816435+DNABM@users.noreply.github.com>
2024-01-08 23:10:17 +0100

    r 16047 A1 Y2 X

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -31989,23 +31989,21 @@ class maddog:
                 and ma50_last < ma59_last
                 and ma50_last < ma50_2_min_ago
                 
-                and ma2_last < ma2_2_min_ago
-                and ma2_differenza_2_min_ago < -8
-                and macd_differenza_3_min_ago < 0.10
-                
                 and deviation_ma39 < 0.09
-                and deviation_sell < 0.02
-                
+                and deviation_sell < -0.04
                 
                 and deviation_ma100_sopra_ma300 < 0.40
                 and deviation_ma5_sotto_ma200 > -0.80
                 
+                and ma2_last < ma2_2_min_ago
+                and ma2_differenza_2_min_ago < -8
+                and macd_differenza_3_min_ago < 0.10
             ):
-                sell = "SELL cond spec con ma10>ma10 2 min ag CON 50<59 e dev 3-39<0.09 e dev_sell < 0.02 e ma50< e macd_diff_3_min_ago<-0.20 e diff macd<-0.30 - r 16047 A1 Y2x"
+                sell = "SELL cond spec con ma10>ma10 2 min ago CON 50<59 e dev 3-39 < 0.09 e dev_sell < -0.04 e ma50< e macd_diff_3_min_ago<-0.20 e diff macd < -0.30 - r 16047 A1 Y2 X"
                 action = "sell"
                 
                 # 22 dic 2023 aggiunta and ma10_last > ma10_2_min_ago (precedentemente ha venduto mentre saliva)
-
+                #  8 gen 2024 deviation_sell a -0.04 da 0.02
             
             
             elif (
