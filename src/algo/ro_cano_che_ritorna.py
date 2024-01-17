60adef8aaefff77c24826a76c50d2cb3a53dd2b2
DNABM <36816435+DNABM@users.noreply.github.com>
2024-01-17 21:08:15 +0100

    prova errore

--- src/algo/ro_cano_che_ritorna.py
+++ src/algo/ro_cano_che_ritorna.py
@@ -1,7 +1,7 @@
+ A.D. 2024 
+# sono 20 anni compa !
 class ro_cano_che_ritorna:
 
-# A.D. 2024 
-# sono 20 anni compa !
   
     def __init__(self, helper, buy_percentage, sell_percentage):
         self.algo_helper = helper
