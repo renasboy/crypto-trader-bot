91b24a4e1e1074dbd803257335a6b4fc7f7f2e82
DNABM <36816435+DNABM@users.noreply.github.com>
2024-01-18 08:50:29 +0100

    r 16047 A2 X1x

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -34345,10 +34345,38 @@ class maddog:
                 
                 # 14 gen 2024 and deviation_sell < 0.01 da 0.005
                 # 14 gen 2024 and deviation_ma2_sopra_o_sotto_ma2_2_min_ago < -0.018
+
             
             elif (
                 
                 ma50_last > ma50_2_min_ago
+                and ma20_last > ma2_20_min_ago
+                and ma100_last > ma100_60_min_ago
+                and ma3_last < ma28_last
+                
+                and deviation_ma39 < 0.13
+                and deviation_sell < -0.01
+                
+                and deviation_ma5_sotto_ma200 > -0.60
+                and deviation_ma100_sopra_ma300 < 0.40
+                
+                and ma2_last < ma2_2_min_ago 
+                and macd < macd_2_min_ago
+                and macd < 15
+                and macd_differenza_2_min_ago < -2
+                and ma2_last < ma5_last
+                and ma2_differenza_2_min_ago < -3
+            ):
+                sell = "SELL nuova cond spec - se 100> vende con 3-28 dev 3-39 < 0.13 e dev_sell < 0.01 e ma50 > e con ma5 sotto ma200 > -0.60 e MACD < and macd < 15 - r 16047 A2 X1x"
+                action = "sell"
+                
+                # 18 gen 2024 se ma20 > ma20 2 min ago dev sell a -0.01 da 0.02
+                
+            
+            elif (
+                
+                ma50_last > ma50_2_min_ago
+                and ma20_last < ma2_20_min_ago
                 and ma100_last > ma100_60_min_ago
                 and ma3_last < ma28_last
                 
@@ -34365,8 +34393,10 @@ class maddog:
                 and ma2_last < ma5_last
                 and ma2_differenza_2_min_ago < -3
             ):
-                sell = "SELL nuova cond spec - se 100> vende con 3-28 dev 3-39 < 0.13 and dev_sell < 0.02 e ma50 > e con ma5 sotto ma200 > -0.60 e MACD < and macd < 15 - r 16047 A2X1"
+                sell = "SELL nuova cond spec - se 100> vende con 3-28 dev 3-39 < 0.13 e dev_sell < 0.02 e ma50 > e con ma5 sotto ma200 > -0.60 e MACD < and macd < 15 - r 16047 A2 X1y"
                 action = "sell"
+
+          
 
             elif (
                 
