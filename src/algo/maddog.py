62b49971d8918d7d7937535a171f37754791b63f
DNABM <36816435+DNABM@users.noreply.github.com>
2024-01-19 16:56:32 +0100

    r 1204 NUOVA ERA A2 SOFT con 5-50

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -2395,7 +2395,7 @@ class maddog:
                     and ma100_last < ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
                     and deviation_ma5_sopra_o_sotto_ma100 < -0.40
-                    and ma8_last > ma54_last
+                    and ma5_last > ma50_last
 
                     
                     and ma100_last < ma100_60_min_ago
