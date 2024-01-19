f5210ac44938bdce49c82f80af983ad439ab10ce
DNABM <36816435+DNABM@users.noreply.github.com>
2024-01-19 09:52:24 +0100

    tutto ok

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1646,7 +1646,7 @@ class maddog:
                     ma3_last > ma18_last
                     and ma100_last < ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 > -0.30
+                    and deviation_ma5_sopra_o_sotto_ma100 > -0.52
                     
                     and deviation_ma5_sopra_o_sotto_ma100 > 0.28
                     
@@ -1683,7 +1683,7 @@ class maddog:
                     ma3_last > ma18_last
                     and ma100_last < ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 < -0.30
+                    and deviation_ma5_sopra_o_sotto_ma100 < -0.52
                     
                     and ma450_last < ma450_360_min_ago
                     and ma300_last < ma300_60_min_ago
@@ -1718,7 +1718,7 @@ class maddog:
                     ma3_last > ma18_last
                     and ma100_last > ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 > -0.30
+                    and deviation_ma5_sopra_o_sotto_ma100 > -0.52
                     
                     and ma450_last < ma450_360_min_ago
                     and ma300_last < ma300_60_min_ago
@@ -1753,7 +1753,7 @@ class maddog:
                     ma3_last > ma18_last
                     and ma100_last > ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 < -0.30
+                    and deviation_ma5_sopra_o_sotto_ma100 < -0.52
                     
                     and ma450_last < ma450_360_min_ago
                     and ma300_last < ma300_60_min_ago
@@ -1798,7 +1798,7 @@ class maddog:
                     ma3_last > ma18_last
                     and ma100_last < ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago < -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 > -0.30
+                    and deviation_ma5_sopra_o_sotto_ma100 > -0.52
                     and ma5_last > ma54_last
                     
                     and ma450_last < ma450_360_min_ago
@@ -1834,7 +1834,7 @@ class maddog:
                     ma3_last > ma18_last
                     and ma100_last < ma100_10_min_ago
                     and deviation_ma100_sopra_o_sotto_ma100_10_min_ago < -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 < -0.30
+                    and deviation_ma5_sopra_o_sotto_ma100 < -0.52
                     and ma5_last > ma54_last
                     
                     and ma450_last < ma450_360_min_ago
