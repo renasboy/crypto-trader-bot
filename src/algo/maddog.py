2787578191b2cd3fa53f3e25634ce3e40dcc5f25
DNABM <dnastocktrading@yahoo.com>
2024-02-21 11:50:40 +0100

    r 8552 y 1 e 2

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -25905,10 +25905,40 @@ class maddog:
                         action = "sell"
                         
                         # 12 feb 2024 se 10>20 and deviation_sell a -0.01 da 0.02
-                    
+
+
                     elif (
                         ma78_last < ma78_120_min_ago
                         and ma300_last < ma300_301_min_ago
+                        and ma10_last > ma30_last
+                        and ma10_last < ma20_last
+                        and deviation_ma39 < 0.13
+                        and deviation_sell < 0.005
+                        
+                        and ma50_last <= ma50_2_min_ago
+                        and deviation_ma100_sopra_ma300 < 0.40
+                
+                        and deviation_ma5_sotto_ma200 > -0.90
+                
+                        and ma2_last <= ma2_2_min_ago 
+                        and macd < macd_2_min_ago
+                        and deviation_ma2_sopra_o_sotto_ma2_2_min_ago < -0.015
+                    ):
+                        sell = "SELL 1 (12-21 min) EX cond.spec. r 16047 B2 con ma300> 5 ore E dev 3-39 < 0.13 e dev_sell < 0.005 e ma50 < e ma5 sotto ma200 > -0.90 - r 8552 y1"
+                        action = "sell"
+                        
+                        # 25 ott 2023 and deviation_sell < -0.069 da < -0.06
+                        
+                        # 27 nov 2023 dev sell a 0.02 da -0.069
+                        # 27 nov 2023 dev 39 a 0.13 da 0.05 INCREDIBILE ! 3-39 STAVA A 0.13 ! quando doveva vendere ! nonno mario.
+                        # 11 dic 2023 aggiunta and deviation_ma2_sopra_o_sotto_ma2_2_min_ago < -0.015 che sarebbero 6 pts !
+                        # 21 feb 2024 se 10 > 30 aumento dev sell a 0.05 da 0.02
+
+
+                    elif (
+                        ma78_last < ma78_120_min_ago
+                        and ma300_last < ma300_301_min_ago
+                        and ma10_last < ma30_last
                         and ma10_last < ma20_last
                         and deviation_ma39 < 0.13
                         and deviation_sell < 0.02
@@ -25922,7 +25952,7 @@ class maddog:
                         and macd < macd_2_min_ago
                         and deviation_ma2_sopra_o_sotto_ma2_2_min_ago < -0.015
                     ):
-                        sell = "SELL 1 (12-21 min) EX cond.spec. r 16047 B2 con ma300> 5 ore E dev 3-39 < 0.13 e dev_sell < 0.02 e ma50 < e ma5 sotto ma200 > -0.90 - r 8552 y"
+                        sell = "SELL 1 (12-21 min) EX cond.spec. r 16047 B2 con ma300> 5 ore E dev 3-39 < 0.13 e dev_sell < 0.02 e ma50 < e ma5 sotto ma200 > -0.90 - r 8552 y2"
                         action = "sell"
                         
                         # 25 ott 2023 and deviation_sell < -0.069 da < -0.06
