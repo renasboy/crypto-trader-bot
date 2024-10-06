dab6be82809e78c41d8c8c76c5f6ffeb59fc490d
DNABM <dnastocktrading@yahoo.com>
2024-10-06 19:06:30 +0200

    r 7144 A1 Y2 NUOVA ERA A3 SOFT

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -23875,32 +23875,28 @@ class maddog:
                 
                 elif (    
                     ma3_last > ma20_last
+                    and ma450_last >= ma450_120_min_ago
+                    and ma100_last > ma450_last
+
                     
-                    and ma100_last > ma100_5_min_ago
-                    and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
+                    and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.20
                     and deviation_ma5_sopra_o_sotto_ma100 > -0.50
 
                     and ma10_last < ma30_last
-                    and ma50_differenza_ma59 < -5
-                    
-                    and ma450_last >= ma450_120_min_ago
-                    and ma50_last > ma450_last
+                   
                     
                     and deviation_ma3_sopra_ma10 > 0.01
-                    and deviation_ma3_sopra_ma16 > 0.02
-                    and deviation_ma5_sopra_ma28 > -0.02
+                    and deviation_ma3_sopra_ma16 > -0.02
+                    
                     
                     and ma3_last > ma13_last
                     and ma3_last > ma5_last
                     
                     and ma2_last > ma2_2_min_ago
-                    and deviation_ma2_sopra_o_sotto_ma2_2_min_ago > 0.021
-                    and ma2_differenza_2_min_ago > 8
-                    and ma3_last >= ma3_2_min_ago
-                    and ma2_differenza_ma5 > 8
-                    and macd_differenza_2_min_ago > 0.30
+                    and macd_differenza_2_min_ago > -3.25
+                
                 ):
-                    buy = "BUY 5 con 3-20 and 450_last >= 450_120_min_ago and 5-28 > -0.02 CON 50 > 450 and ma2_differenza_ma5 > 8 - r 7144 A1 Y2 NUOVA ERA A3 SOFT"
+                    buy = "BUY 5 con 450_last >= 450_120_min_ago E ma100>ma450 - r 7144 A1 Y2 NUOVA ERA A3 SOFT"
                     action = "buy"
                     percentage = 70
                     
@@ -23908,6 +23904,7 @@ class maddog:
                     #  7 feb 2024 aggiunto and ma3_last > ma5_last
                     #  7 feb 2024 aggiunta and ma3_last >= ma3_2_min_ago
                     #  9 feb 2024 IMPORTANTE ! solo BUY 5 NUOVA ERA A3 SOFT con ma100> ma100 5 min ago !!!
+                    #  6 ott 2024 ANTICIPATA ! anche ma100 > ma450 ! a chi cazzo aspetti !
                 
                 elif (    
                     ma3_last > ma20_last
