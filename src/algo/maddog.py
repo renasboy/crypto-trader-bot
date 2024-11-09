b8ed6690e5685a14b5a00e895d4740b80e93cf46
DNABM <dnastocktrading@yahoo.com>
2024-11-09 15:15:06 +0100

    r 16047 A2 X2 Y1

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -45770,14 +45770,15 @@ class maddog:
                 #  2 feb 2024 and deviation_sell < -0.02 da 0.02
 
           
-            
             elif (    
                 ma50_last > ma50_2_min_ago
+                and deviation_ma50_sopra_o_sotto_ma300 > 0.10
+
                 and ma100_last > ma100_60_min_ago
                 and ma3_last < ma28_last
                 
                 and deviation_ma39 < 0.13
-                and deviation_sell < 0.01
+                and deviation_sell < -0.02
                 
                 and deviation_ma5_sotto_ma200 > -0.70
                 and deviation_ma100_sopra_ma300 < 0.40
@@ -45789,7 +45790,32 @@ class maddog:
                 and macd < 1
                 and macd_differenza_2_min_ago < -1.00
             ):
-                sell = "SELL CS se 100> vende con 3-28 e dev 3-39 < 0.13 and dev_sell < 0.01 e ma50 > con MACD IN TERRITORIO NEG and DIFF MACD < -1 - r 16047 A2 X2 Y"
+                sell = "SELL CS se 100> vende con 3-28 e dev 3-39 < 0.13 and dev_sell < -0.02 e ma50 > con DIFF MACD < -1 - r 16047 A2 X2 Y1"
+                action = "sell"
+
+                #  9 nov 2024 se deviation_ma50_sopra_o_sotto_ma300 > 0.10 allora and deviation_sell < -0.02 da < 0.01
+
+
+            elif (    
+                ma50_last > ma50_2_min_ago
+                and deviation_ma50_sopra_o_sotto_ma300 < 0.10
+                and ma100_last > ma100_60_min_ago
+                and ma3_last < ma28_last
+                
+                and deviation_ma39 < 0.13
+                and deviation_sell < -0.01
+                
+                and deviation_ma5_sotto_ma200 > -0.70
+                and deviation_ma100_sopra_ma300 < 0.40
+                
+                and ma2_last < ma5_last
+                and ma2_last < ma2_2_min_ago
+                and deviation_ma2_sopra_o_sotto_ma2_2_min_ago < -0.017
+                
+                and macd < 1
+                and macd_differenza_2_min_ago < -1.00
+            ):
+                sell = "SELL CS se 100> vende con 3-28 e dev 3-39 < 0.13 and dev_sell < -0.01 e ma50 > con DIFF MACD < -1 - r 16047 A2 X2 Y2"
                 action = "sell"
                 
                 # 29 nov 2023 vende con 3-28 se ma 100 > 100 60 min
@@ -45800,6 +45826,7 @@ class maddog:
                 # 29 dic 2023 ATTENZIONE ! macd diff < -1 significa CHE HA PIU' FORZA mentre scende ! scende piu' con vigore !
                 # 30 dic 2023 situazione MACD in territorio negativo
                 # 16 set 2024 and dev_sell < 0.01 da < 0.02
+                #  9 nov 2024 dev sell a -0.01 da 0.01
 
 
             elif (
