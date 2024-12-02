5cc15bdd7aaee7da5a4a5d3a4a8351073e74b07e
DNABM <dnastocktrading@yahoo.com>
2024-12-02 12:08:52 +0100

    r 7270 NUOVA ERA A3 SOFT

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -25696,30 +25696,38 @@ class maddog:
 
                 elif (    
                     ma3_last > ma20_last
-                    
-                    and ma100_last > ma100_5_min_ago
-                    and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
-                    and deviation_ma5_sopra_o_sotto_ma100 > -0.50
-                    
-                    and ma300_last > ma300_180_min_ago
-                    and deviation_ma300_sopra_ma450 > -0.50
-                    
+
+                    and ma39_last < ma100_last
+                    and deviation_ma25_sotto_ma300 < -0.16
+                    and deviation_ma50_sotto_ma300 < -0.20
+                    and deviation_ma39 < 0.08
+                    and deviation_ma3_sotto_ma50 < 0.08
+                    and deviation_ma5_sopra_o_sotto_ma100 < -0.05
+                    and deviation_ma5_sotto_ma200 < -0.10
+                    and deviation_ma5_sotto_ma300 < -0.10
+                    and deviation_ma5_sotto_ma450 < -0.10
+
+
                     and ma100_last < ma100_120_min_ago
+                    and deviation_ma100_sopra_o_sotto_ma100_10_min_ago > -0.12
                     and deviation_ma54_sopra_o_sotto_ma72 < -0.015
                     and deviation_ma39_sotto_ma200 < -0.01
-                    
-                    and deviation_ma3_sopra_ma10 > 0.017
-                    and deviation_ma3_sopra_ma16 > 0.025
+
+                    and ma300_last > ma300_180_min_ago
+                    and deviation_ma300_sopra_ma450 > -0.50
+
+                    and deviation_ma3_sopra_ma10 > 0.01
+                    and deviation_ma3_sopra_ma16 > -0.01
                     and deviation_ma3_sopra_ma18 > -0.03
                     
                     and ma2_last >= ma2_2_min_ago
                     and ma2_differenza_2_min_ago > 3
-                    and ma2_differenza_ma5 > 26
-                    and macd > -10
+                    
+                    and macd > -50
                     and macd >= macd_2_min_ago
-                    and ma2_last >= ma20_last
+                    
                 ):    
-                    buy = "BUY 5 da r 1537 A3X 4A compare forever + 300>180 min con 3-20 E 300 vicina 450 e 3-16 > 0.01 + MACD > - r 7270 NUOVA ERA A3 SOFT"
+                    buy = "BUY 5 CF + 300>180 min con 3-20 E 300 vicina 450 e 3-16 > 0.01 + MACD > + DNAM - r 7270 NUOVA ERA A3 SOFT"
                     action = "buy"
                     percentage = 90
                     
