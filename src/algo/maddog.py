816487c913f823051f432cf14839879101e73f25
DNABM <dnastocktrading@yahoo.com>
2025-02-22 21:06:33 +0100

    r 16047 B3 y1

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -49980,9 +49980,12 @@ ma3_last > ma20_last
                 # 13 gen 2024 se 10 va sopra 20 and deviation_sell < -0.10
                 # 19 gen 2024 deviation_sell < -0.10 ha fatto -0.40 !
                 # 19 gen 2024 deviation_sell a -0.05 da -0.10
-            
+
+
+
             elif (
                 ma78_last < ma78_120_min_ago
+                and ma5_last > ma100_last
                 and ma10_last <= ma20_last
                 and ma2_differenza_2_min_ago < -7
                 
@@ -50000,14 +50003,41 @@ ma3_last > ma20_last
                 and ma2_last <= ma2_2_min_ago 
                 and macd < macd_2_min_ago
             ):
-                sell = "SELL condizione speciale 6 NOTTE con ma300> 5 ore - SALVAGENTE 4 dev 3-39 < 0.05 and dev_sell < -0.083 con ma50 < e con ma5 sotto ma200 > -0.90 - r 16047 B3y"
+                sell = "SELL CS 6 NOTTE con ma300> 5 ore - dev 3-39 < 0.05 e dev_sell < -0.083 e ma50 < e e ma5 > ma 100 - r 16047 B3 y1"
+                action = "sell"
+
+                # 22 feb 2025 ti ricordo che ma5 sotto ma200 > -0.90 significa che non e' una situazione crollo !
+
+            elif (
+                ma78_last < ma78_120_min_ago
+                and ma5_last <= ma100_last
+                and ma10_last <= ma20_last
+                and ma2_differenza_2_min_ago < -7
+                
+                and ma300_last > ma300_301_min_ago
+                and delta_150_100 <= delta_150_100_60_min
+                
+                and deviation_ma39 < 0.05
+                and deviation_sell < -0.023
+                
+                and ma50_last <= ma50_2_min_ago
+                and deviation_ma100_sopra_ma300 < 0.40
+                
+                and deviation_ma5_sotto_ma200 > -0.90
+                
+                and ma2_last <= ma2_2_min_ago 
+                and macd < macd_2_min_ago
+            ):
+                sell = "SELL CS 6 NOTTE con ma300> 5 ore - dev 3-39 < 0.05 e dev_sell < -0.023 e ma50 < e ma5 < ma 100 - r 16047 B3 y2"
                 action = "sell"
                 
                 # 15 set 2023 quando 300 scende da 5 ore non devi vendere in perdita ! a ricomprare ci penseranno altre condizioni !
                 # 17 nov 203 aggiunto and ma2_differenza_2_min_ago < -5
-                # 24 nov 203 ma2_differenza_2_min_ago < -7 da -5 VENDE MENTRE MA2 E' IN CRESCITA ! credo che sia una questione di CANDELE allora aumento un po' la differenza
+                # 24 nov 203 ma2_differenza_2_min_ago < -7 da -5 VENDE MENTRE MA2 E' IN CRESCITA ! credo che sia una questione di CANDELE.
                 # 24 nov 203 dev sell 0.083 da -0.079
-          
+                # 22 feb 2025 se 5 e' andata sotto 100 allora deviation_sell a -0.023 da -0.083
+                # 22 feb 2025 ti ricordo che ma5 sotto ma200 > -0.90 significa che non e' una situazione crollo !
+
                 #########################################################################################################################
                 
                 
