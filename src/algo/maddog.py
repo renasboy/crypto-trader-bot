6bc343e703534e084d692ece50c6d97d166283e1
DNABM <dnastocktrading@yahoo.com>
2025-02-24 11:32:32 +0100

    sono tornato alla vecchia gabbia nel tentativo di sbloccare

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -1739,9 +1739,10 @@ class maddog:
 
 
 
-        if (deviation_ma300_sopra_ma450 > -0.01 and deviation_ma300_sopra_ma450 < -0.20 and delta_1 > delta_2) or (deviation_ma300_sopra_ma450 >= -0.20 and deviation_ma300_sopra_ma450 < -0.20 and delta_1 < delta_2):
+        # if (deviation_ma300_sopra_ma450 > -0.01 and deviation_ma300_sopra_ma450 < -0.20 and delta_1 > delta_2) or (deviation_ma300_sopra_ma450 >= -0.20 and deviation_ma300_sopra_ma450 < -0.20 and delta_1 < delta_2):
 
-
+        if deviation_1_gabbia > -0.27 or deviation_buy_crollo_1 < -1.51 or deviation_buy_crollo_1 > -1.50 and deviation_buy_crollo_1 < -0.60 or deviation_buy_crollo_1 > -0.59 and deviation_buy_crollo_1 < -0.27:
+ 
 
 
 
