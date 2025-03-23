b6777712e41341ef9e81d3d26d13c0f4a374e16d
DNABM <dnastocktrading@yahoo.com>
2025-03-23 18:28:53 +0100

    r 15985 A1 ok 4y 1a Y1

--- src/algo/maddog.py
+++ src/algo/maddog.py
@@ -48090,11 +48090,13 @@ class maddog:
             elif (
                 ma50_last > ma50_2_min_ago
                 and ma200_last > ma450_last
-                and ma28_last < ma100_last
                 and ma10_last > ma20_last
+
+                and ma28_last < ma100_last
                 
-                and deviation_ma39 < 0.05
-                and deviation_sell < 0.001
+                
+                and deviation_ma39 < 0.02
+                and deviation_sell < -0.01
                 
                 and deviation_ma100_sopra_ma300 > 0.20
                 and delta_450_300 < delta_450_300_60_min
@@ -48102,14 +48104,13 @@ class maddog:
                 and deviation_ma5_sopra_o_sotto_ma5_1080_min_ago < 1.50
                 and deviation_ma5_sopra_o_sotto_ma5_1080_min_ago > -1.50
                 
-                and deviation_ma10_sopra_o_sotto_ma10_30_min_ago < 0.10
                 and deviation_ma3_sotto_ma150 > -1.20
                 
-                and macd_differenza_2_min_ago < -0.5
-                and ma2_last < ma5_last
-                and price < ma3_last
+                
+                and ma2_last < ma8_last
+                and price < ma8_last
             ):
-                sell = "SELL CS con dev 3-39 < 0.05 e dev sell < 0.001 con ma50< MA 200>450 e price < ma3 - r 15985 A1 ok 4y 1a Y1"
+                sell = "SELL CS con dev 3-39 < 0.05 e dev sell < 0.001 con ma50< MA 200>450 e price < ma8 - r 15985 A1 ok 4y 1a Y1"
                 action = "sell"
                 
                 # 27 dic 2023 con 10>20 deviation_sell a 0.001 da 0.02
@@ -48117,7 +48118,7 @@ class maddog:
                 # 24 set 2024 and ma28_last > ma100_last
                 # 12 mar 2025 aggiunta opzione ma50_last > ma50_2_min_ago (ha fatto -0.60)
                 # 12 mar 2025 and deviation_sell < 0.001 da < -0.002 (ha fatto -0.60)
-
+                # 23 mar 2025 and deviation_sell a 0.01 da 0.001 togliendo un po' di sovrastrutture  !
 
 
 
